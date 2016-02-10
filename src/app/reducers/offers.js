ac2fb9c8f09bbcc4b27ff480b6e6a0799945e2bf
Benjamin Menant <dev@menant-benjamin.fr>
2016-02-10 21:21:54 +0100

    add quechoisir offer

--- src/app/reducers/offers.js
+++ src/app/reducers/offers.js
@@ -29,7 +29,27 @@ const initialState = [
             }
         ],
         description: 'Allez voir des trucs open source'
-    }
+    },
+    {
+        matchingContext: {
+            url: "http://www.amazon.fr/Beko-WML-15106-NE-linge/dp/B006MHD5QG",
+        },
+        alternatives: [
+            {
+                url: "http://www.amazon.fr/Beko-WML-15106-NE-linge/dp/B006MHD5QG",
+                call_to_action: "Voir le classement et les alternatives",
+                description: "Dans notre classement du 2 février 2016, le lave-linge Beko WML 15106 NE a terminé 134ème sur 134 lave-linges testés. Beaucoup d’alternatives moins chères ont eu une bien meilleure évaluation lors de nos tests.",
+                tags: [
+                    "price",
+                    "quality",
+                ],
+                contributor: {
+                    id: 'quechoisir',
+                    description: 'L’UFC-Que Choisir est une association à but non lucratif, au service des consommateurs.',
+                },
+            },
+        ],
+    },
 ];
 
 export default function offers(state = initialState, action) {
