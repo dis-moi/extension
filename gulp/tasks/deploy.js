13d4483efabe2706f042ac731a2587f90ffc15a3
Vincent Van Steenbergen <v.vansteenbergen@gmail.com>
2016-07-06 14:19:48 +0200

    Link build and deploy tasks

--- gulp/tasks/deploy.js
+++ gulp/tasks/deploy.js
@@ -6,7 +6,6 @@ gulp.task('sftp:deploy:extension', function () {
         .pipe(sftp({
           host: 'sftp.dc0.gpaas.net',
           user: '3249456',
-          keyLocation: '/home/vincent/.ssh/id_rsa_old',
           remotePath: '/lamp0/web/vhosts/ui.lmem.net/htdocs/'
         }));
 });
