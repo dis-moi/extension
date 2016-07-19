220205627b738c88567e11eefd7d0a3ec9f11f0a
David Bruant <bruant.d@gmail.com>
2016-07-19 17:47:40 +0200

    Tests can be run from background by calling window.integrationTest()

--- test/integration/index.js
+++ test/integration/index.js
@@ -4,6 +4,35 @@ mocha.setup({
     reporter: WebConsole
 })
 
+chai.use(chaiAsPromised);
+
+const expect = chai.expect;
+
+describe('yep', () => {
+
+    it('should succeed with 5 tabs', () => {
+        const tabsP = new Promise(resolve => {
+             chrome.tabs.query({}, resolve); 
+        })
+
+        return Promise.all([
+            expect(tabsP).to.eventually.be.an.Array,
+            expect(tabsP).to.eventually.have.length(5)
+        ])
+    });
+
+    it('should fail with 5 tabs', () => {
+        const tabsP = new Promise(resolve => {
+             chrome.tabs.query({}, resolve); 
+        })
+
+        return Promise.all([
+            expect(tabsP).to.eventually.be.an.Array,
+            expect(tabsP).to.eventually.have.length(10)
+        ])
+    });
+})
+
 export default function(){
     console.log('mocha.run()')
     mocha.run()
