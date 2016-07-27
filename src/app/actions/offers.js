c077f09540288ac31cf19b77f03d9bf90f527bc0
David Bruant <bruant.d@gmail.com>
2016-07-27 12:37:50 +0200

    Remove popup and related constants

--- src/app/actions/offers.js
+++ src/app/actions/offers.js
@@ -1,21 +1,9 @@
-import { MATCHING_OFFERS_FOUND, REMOVE_ALL_OFFERS, REMOVE_ALL_MATCHING_OFFERS } from './../constants/ActionTypes';
+import { MATCHING_OFFERS_FOUND } from './../constants/ActionTypes';
 
 
 export function matchingOffersFound(details) {
     return {
         type: MATCHING_OFFERS_FOUND,
         payload: details
-    };
-}
-
-export function flushMatchingOffers(){
-    return {
-        type: REMOVE_ALL_MATCHING_OFFERS
-    };
-}
-
-export function flushOffers() {
-    return {
-        type: REMOVE_ALL_OFFERS
     };
 }
