20cfcd77c56996e1a86f04c493919d3abe5c5f75
David Bruant <bruant.d@gmail.com>
2016-08-17 14:58:06 +0200

    Warn user about Node <6

--- test/infrastructure/neverThrowingObject.js
+++ test/infrastructure/neverThrowingObject.js
@@ -1,3 +1,9 @@
+
+if(typeof Proxy === 'undefined'){
+  console.warn("The standard Proxy built-in is not defined. You need Node v6+. You're using version", process.version)
+  process.exit(-1);
+}
+
 export default function(){
   const target = function () { return proxy; };
   const handler = {
