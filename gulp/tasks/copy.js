b36b7de792563b9900c35366fe9583fdf4786394
David Bruant <bruant.d@gmail.com>
2016-09-07 15:44:35 +0200

    Generate manifest files from code sharing a base file

--- gulp/tasks/copy.js
+++ gulp/tasks/copy.js
@@ -1,10 +1,13 @@
 import gulp from 'gulp';
-import rename from 'gulp-rename';
+import file from 'gulp-file';
 
-const copy = (dest, manifest, tests) => () => {
-  if (manifest) {
-    gulp.src(`./src/${manifest}`)
-      .pipe(rename('manifest.json'))
+import devManifest from '../../src/browser/extension/manifest/dev';
+import prodManifest from '../../src/browser/extension/manifest/prod';
+import stagingManifest from '../../src/browser/extension/manifest/staging';
+
+const copy = (dest, manifestStr, tests) => () => {
+  if (manifestStr) {
+    file('manifest.json', manifestStr, { src: true })
       .pipe(gulp.dest(dest));
   }
   gulp.src('./src/assets/**/*').pipe(gulp.dest(dest));
@@ -16,14 +19,14 @@ const copy = (dest, manifest, tests) => () => {
   }
 };
 
-const manifestDevSource = 'browser/extension/manifest.dev.json';
 
-gulp.task('copy:build:dev', copy('./build/dev', manifestDevSource, true));
+gulp.task('copy:build:dev',
+  copy('./build/dev', JSON.stringify(devManifest, null, 2), true));
 gulp.task('copy:build:staging',
-  copy('./build/staging', 'browser/extension/manifest.staging.json'));
+  copy('./build/staging', JSON.stringify(stagingManifest, null, 2)));
 gulp.task('copy:build:production',
-  copy('./build/production', 'browser/extension/manifest.prod.json'));
+  copy('./build/production', JSON.stringify(prodManifest, null, 2)));
 
 gulp.task('copy:watch', () => {
-  gulp.watch(`./src/${manifestDevSource}`, ['copy:build:dev']);
+  gulp.watch(`./src/browser/extension/manifest`, ['copy:build:dev']);
 });
---
+++ src/browser/extension/manifest/base.js
@@ -0,0 +1,42 @@
+/**
+ * This file is not meant to be imported directly as a manifest file.
+ * Instead it should be imported by other files which contains all the missing manifest fields
+ * which values are environment-specific.
+ * See other files in the directory
+ */
+
+export default Object.freeze({
+  'version': '0.1.0',
+  'manifest_version': 2,
+  'description': 'LMEM',
+  'browser_action': {
+    'default_icon': 'img/logo/48x48.png',
+    'default_title': 'LMEM',
+    'default_popup': 'popup.html'
+  },
+  'icons': {
+    '16': 'img/logo/16x16.png',
+    '48': 'img/logo/48x48.png',
+    '128': 'img/logo/128x128.png'
+  },
+  'background': {
+    'page': 'background.html'
+  },
+  'permissions': [
+    'contextMenus',
+    'privacy',
+    'storage',
+    'tabs',
+    'unlimitedStorage',
+    'webNavigation',
+    'webRequest',
+    'webRequestBlocking',
+    'http://*/*',
+    'https://*/*'
+  ],
+  'web_accessible_resources': [
+    'img/*',
+    'styles/*',
+    'fonts/*'
+  ]
+});
