5730e913455cf99fb4696ab520bd38ee5253be16
Benjamin Menant <dev@menant-benjamin.fr>
2016-09-17 12:29:09 +0200

    fix dateOfInstall when missing (i.e. install prior to this feature release)

--- src/app/components/PreferenceAboutPanel.js
+++ src/app/components/PreferenceAboutPanel.js
@@ -1,17 +1,28 @@
 import React, { Component, PropTypes } from 'react';
 import { EXTENSION_VERSION } from '../constants/ui';
 
-export default function ({ onInstalledDetails }) {
-  const dateOfInstall = new Date(onInstalledDetails.get('datetime'));
-  const localeDateOfInstall = dateOfInstall.toLocaleDateString('fr',
+function formatLocaleDate(strDate) {
+  const dateOfInstall = new Date(strDate);
+
+  if (isNaN(dateOfInstall.valueOf()))
+    return undefined;
+
+  return dateOfInstall.toLocaleDateString('fr',
     { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
+}
+
+export default function ({ onInstalledDetails }) {
+  const ISODateOfInstall = onInstalledDetails && onInstalledDetails.get('datetime');
+  const localeDateOfInstall = ISODateOfInstall && formatLocaleDate(ISODateOfInstall);
 
   return (
     <div>
       <section>
         <h1>
-          Le Même en Mieux est un assistant d’achat que vous avez installé le&nbsp;
-          <time dateTime={dateOfInstall.toISOString()}>{localeDateOfInstall}</time>.
+          <span>Le Même en Mieux est un assistant d’achat</span>
+          { localeDateOfInstall ? (
+            <span>que vous avez installé le <time dateTime={ISODateOfInstall}>{localeDateOfInstall}</time></span>
+          ) : ''}.
         </h1>
         <p>
           Quand vous consultez un produit sur Internet, il vous trouve des conseils d’achat,
