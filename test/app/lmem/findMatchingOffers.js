855683d7375552269739abb74e615e4be1ab2c04
Benjamin Menant <dev@menant-benjamin.fr>
2016-10-12 17:21:19 +0200

    test try/catch regex test with matchingContext: ref #80

---
+++ test/app/lmem/findMatchingOffers.js
@@ -0,0 +1,53 @@
+import chai from 'chai';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+
+import findMMCAccordingToPreferences from '../../../src/app/lmem/findMatchingOffersAccordingToPreferences';
+
+const expect = chai.expect;
+chai.use(sinonChai);
+
+const offers = [
+  {url_regex: 's.*'},
+  {url_regex: 'SamSung'},
+  {url_regex: 'doesNotMatch'}
+];
+
+const matchingURL = 'https://samsung.com/blabla';
+
+describe('findMatchingOffers', function () {
+
+  it('should be case insensitive', () => {
+    const matches = findMMCAccordingToPreferences(matchingURL, offers, []);
+
+    expect(matches).to.be.an('array');
+    expect(matches).to.be.of.length(2);
+    expect(matches[0]).to.equal(offers[0]);
+    expect(matches[1]).to.equal(offers[1]);
+  });
+
+  describe('invalid regex', () => {
+    const nastyOffers = [{url_regex: 'isNasty)'}].concat(offers);
+
+    beforeEach(() => {
+      sinon.stub(console, 'error');
+    });
+
+    afterEach(() => {
+      console.error.restore();
+    });
+
+    it('should not screw up the matching engine', () => {
+      const matches = findMMCAccordingToPreferences(matchingURL, nastyOffers, []);
+
+      expect(console.error).to.have.been.calledWith('MatchingContext ignored:');
+
+      expect(matches).to.be.an('array');
+      expect(matches).to.be.of.length(2);
+      expect(matches[0]).to.equal(nastyOffers[1]);
+      expect(matches[1]).to.equal(nastyOffers[2]);
+    });
+
+  });
+
+});
