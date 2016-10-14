4f17409b2447a960869d1819bcbe5f457152308d
David Bruant <bruant.d@gmail.com>
2016-10-14 13:22:41 +0200

    alternativeFound failing test was a property which changed of name in  the code but not in the tests. Fixed

--- src/app/content/actions/alternatives.js
+++ src/app/content/actions/alternatives.js
@@ -1,6 +1,6 @@
 import { RECOMMENDATION_FOUND } from '../../constants/ActionTypes';
 
-export default function alternativeFound(recommendations) {
+export default function(recommendations) {
   return {
     type: RECOMMENDATION_FOUND,
     recommendations
