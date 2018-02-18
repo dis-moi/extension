6139fa0c9913c789d426921d8eba42767b7ba808
Benjamin Menant <dev@menant-benjamin.fr>
2018-02-18 13:52:38 +0100

    format links in description

--- src/app/content/components/RecoDescription.js
+++ src/app/content/components/RecoDescription.js
@@ -1,12 +1,26 @@
 import React, { PropTypes } from 'react';
 import { sanitize } from 'dompurify';
 
+// FIXME This might be a backend thing...
+function addHtmlLinks(html) {
+  return html.replace(
+    /(https?:\/\/)?([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?/gi,
+    (match, protocol) =>
+      `<a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="${!protocol && 'https://'}${match}?utm_source=lmem_assistant">
+          ${match}
+      </a>`
+  );
+}
+
 function sanitizeHtml(rawHtml) {
   return {
-    __html: sanitize(rawHtml, {
+    __html: addHtmlLinks(sanitize(rawHtml, {
       ALLOWED_TAGS: ['p'],
       ALLOWED_ATTR: [],
-    }),
+    })),
   };
 }
 
