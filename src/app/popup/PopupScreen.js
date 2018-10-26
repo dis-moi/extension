1c4c8c3df068711a6ad0150196a9b615ea10c524
lutangar <johan.dufour@gmail.com>
2018-10-26 09:36:41 +0200

    refactor: use prop-types dedicated package

--- src/app/popup/PopupScreen.js
+++ src/app/popup/PopupScreen.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export default function PopupScreen({ imagesUrl, openPrefScreenAbout, onClick }) {
   function onClickHandler(e) {
