3183a94e281d7453837915d4e5a36128dc61e75c
lutangar <johan.dufour@gmail.com>
2018-10-26 09:24:18 +0200

    BREAKING CHANGE: update weback, loaders and configuration files
    update babel & friends
    update gulp & friends
    update eslint & friedns
    update hot loading solution
    fix lint command
    add analyse command

--- webpack/firefox.config.js
+++ webpack/firefox.config.js
@@ -1,35 +1,30 @@
 import path from 'path';
 import webpack from 'webpack';
 import baseConfig from './base.config';
-const srcPath = path.join(__dirname, '../src/app/');
 
 export default baseConfig({
+  mode: 'production',
   output: {
     path: path.join(__dirname, '../build/firefox'),
     publicPath: '.', // No remote URL with Firefox
   },
-  // No analytics with Firefox
-  loaders: [{
-    test: /lib\/heap/,
-    loader: 'null-loader',
-  }],
+  rules: [
+    {
+      test: /lib\/heap/,
+      use: [{ loader: 'null-loader'}],
+    }
+  ],
   plugins: [
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.UglifyJsPlugin({
-      comments: false,
-      compressor: {
-        warnings: false
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"',
+        LMEM_BACKEND_ORIGIN: '"https://recommendations.lmem.net"',
+        LMEM_SCRIPTS_ORIGIN: '\'.\'', // Use local build
+        ONBOARDING_ORIGIN: '"https://bienvenue.lmem.net?extensionInstalled"',
+        REFRESH_MC_INTERVAL: '10*60*1000',
+        // No analytics with Firefox
+        // HEAP_APPID: '"3705584166"',
       }
     })
   ],
-  globals: {
-    'process.env': {
-      NODE_ENV: '"production"',
-      LMEM_BACKEND_ORIGIN: '"https://recommendations.lmem.net"',
-      LMEM_SCRIPTS_ORIGIN: "'.'", // Use local build
-      ONBOARDING_ORIGIN: '"https://bienvenue.lmem.net?extensionInstalled"',
-      REFRESH_MC_INTERVAL: '10*60*1000',
-      //HEAP_APPID: '"3705584166"', // No analytics with Firefox
-    }
-  },
 });
