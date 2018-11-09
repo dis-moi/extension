795c0898d8a3918c9085967fd4a75a11214066ec
lutangar <johan.dufour@gmail.com>
2018-11-09 15:54:46 +0100

    feat: add style-components stack
    add theme file and example button to get things started
    add stylelint to keep style consistent
    add lint:css command

--- src/app/content/containers/Root.js
+++ src/app/content/containers/Root.js
@@ -2,16 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Provider } from 'react-redux';
 import { hot } from 'react-hot-loader';
+import { ThemeProvider } from 'styled-components';
 import App from './App';
 
-const Root = ({ store }) => (
+const Root = ({ store, theme }) => (
   <Provider store={store}>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </Provider>
 );
 
 Root.propTypes = {
   store: PropTypes.object.isRequired, // eslint-disable-line
+  theme: PropTypes.object.isRequired, // eslint-disable-line
 };
 
 export default hot(module)(Root);
