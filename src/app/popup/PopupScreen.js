c868ac35005d99243a40199d1a522fe157044d37
newick <erick@entre-quote.com>
2018-11-15 17:28:59 +0100

    WIP: added all kind of Bulles

--- src/app/popup/PopupScreen.js
+++ src/app/popup/PopupScreen.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-import CloseButton from '../../components/atoms/CloseButton';
+import ButtonClose from '../../components/atoms/ButtonClose';
+import ButtonMenu from '../../components/atoms/ButtonMenu';
 import Logo from '../../components/atoms/Logo';
-import MenuButton from '../../components/atoms/MenuButton';
 import Title from '../../components/atoms/Title';
 import Type from '../../components/atoms/Type';
 
 import Header from '../../components/molecules/Header';
 import Footer from '../../components/molecules/Footer';
-import Main from '../../components/molecules/Main';
+
+import BulleDetails from '../../components/organisms/BulleDetails';
 
 import Notification from '../../components/views/Notification';
 
@@ -32,13 +33,13 @@ export default function PopupScreen({ imagesUrl, openPrefScreenAbout, onClick })
     <Notification>
       <Header>
         <Title>2 messages pour cette page</Title>
-        <CloseButton>
+        <ButtonClose>
           <svg width="12" height="12">
             <g fill="none" fill-rule="evenodd" stroke="#787993" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
               <path d="M1 1l10 10M11 1L1 11" />
             </g>
           </svg>
-        </CloseButton>
+        </ButtonClose>
       </Header>
       <Header>
         <Title>
@@ -47,32 +48,59 @@ export default function PopupScreen({ imagesUrl, openPrefScreenAbout, onClick })
           </svg> &nbsp;
           Le Même en Mieux, il y a 8 mois
         </Title>
-        <CloseButton>
+        <ButtonClose>
           <svg width="12" height="12">
             <g fill="none" fill-rule="evenodd" stroke="#787993" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
               <path d="M1 1l10 10M11 1L1 11" />
             </g>
           </svg>
-        </CloseButton>
+        </ButtonClose>
       </Header>
       <Header>
         <Title>Aucune de vos relations n’a créé de bulle pour cette page</Title>
-        <CloseButton>
+        <ButtonClose>
           <svg width="12" height="12">
             <g fill="none" fill-rule="evenodd" stroke="#787993" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
               <path d="M1 1l10 10M11 1L1 11" />
             </g>
           </svg>
-        </CloseButton>
+        </ButtonClose>
       </Header>
 
-      <Main>
-        <Type>plop</Type>
-      </Main>
+      <main>
+        <BulleDetails>
+          <Type>plop</Type>
+          <Message>
+          De nombreux clients mécontents de Pixmania et ses vendeurs s’expriment sur les réseaux sociaux depuis 2016. Les plaintes continuent en 2017 et 2018 si l’on se réfère au forum Que Choisir.
+          </Message>
+
+          <SourceURL>http://forum.que-choisir.org/pixmania-avis-1285</SourceURL>
+
+          <Feedbacks>
+            <Approves>21</Approves>
+            <Dislikes>3</Dislikes>
+          </Feedbacks>
+        </BulleDetails>
+
+        <BulleExcerpt>
+          <Type>plop</Type>
+          <Title>« De nombreux clients mécontents de ce… »</Title>
+          <Contributor>Le Même en Mieux</Contributor>
+          <ButtonDelete></ButtonDelete>
+        </BulleExcerpt>
+
+        <BulleDeleted>
+          Cette bulle ne s’affichera plus
+        </BulleDeleted>
+        
+        <BulleToAdd>
+          Ajouter une bulle
+        </BulleToAdd>
+      </main>
 
       <Footer>
         <Logo>Bulles</Logo>
-        <MenuButton>Afficher le menu</MenuButton>
+        <ButtonMenu>Afficher le menu</ButtonMenu>
       </Footer>
     </Notification>
   );
