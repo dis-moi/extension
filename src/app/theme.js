ba5132f98d5d8869a244d9002cc6b70c8dac99b6
newick <erick@entre-quote.com>
2018-11-15 14:05:02 +0100

    feat: tested styled-components

--- src/app/theme.js
+++ src/app/theme.js
@@ -1,3 +1,25 @@
 export default {
-  main: 'purple'
+  main: 'purple',
+
+  // fonts colors
+  mainText: '#25265e',
+  otherText: '#787993',
+
+  // icons colors
+  warningFav: '#FB6C6A',
+  approval: '#2DC76D',
+  alternative: '#488FC1',
+  needHelp: '#F5A623',
+  rant: '#9013FE',
+  sadness: '#D16464',
+  surprise: '#3C6AE2',
+  
+  //icons backgrounds
+  BGWarningFav: '#FEF0F0',
+  BGApproval: '#EAF9F0',
+  BGAlternative: '#F0F5FA',
+  BGNeedHelp: '#FDF9C6',
+  BGRant: '#F3E7FE',
+  BGSadness: '#FAEFEF',
+  BGSurprise: '#EBF0FC'
 };
---
+++ src/components/01-atoms/MenuButton.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export default styled.button`
+    font-size: 13px;
+    color: ${props => props.theme.otherText}
+`;
