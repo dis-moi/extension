d95f71f7d919255d34d24b716758788f625b1540
newick <erick@entre-quote.com>
2018-11-22 16:33:00 +0100

    feat: added index for icons

---
+++ src/components/atoms/icons/Delete.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default () => (
+  <svg width="17" height="17">
+    <g 
+      fill="none" 
+      fillRule="evenodd" 
+      stroke="#787993" 
+      strokeLinecap="round" 
+      strokeLinejoin="round" 
+      strokeLidth="2" 
+      opacity=".5">
+      <path d="M3 7v7a2 2 0 0 0 2 2h7a2 2 0 0 0 2-2V7M6   4V1h5v3M1 4h15v3H1z" />
+    </g>
+  </svg>
+);
