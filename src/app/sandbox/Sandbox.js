c25c0ebbf5283539eed9ba6ac0c978465ce5f577
newick <erick@entre-quote.com>
2018-11-22 14:08:34 +0100

    feat: display all kind of notifications

--- src/app/sandbox/Sandbox.js
+++ src/app/sandbox/Sandbox.js
@@ -3,18 +3,18 @@ import { ThemeProvider } from 'styled-components';
 import { hot, setConfig } from 'react-hot-loader';
 import theme from '../theme';
 
-import { 
-  ButtonClose, 
-  ButtonMenu, 
-  ButtonDelete, 
-  Logo, 
-  Title, 
-  Type, 
-  Message, 
-  SourceURL, 
-  Approves, 
-  Dislikes, 
-  Contributor 
+import {
+  ButtonClose,
+  ButtonMenu,
+  ButtonDelete,
+  Logo,
+  Title,
+  Type,
+  Message,
+  SourceURL,
+  Approves,
+  Dislikes,
+  Contributor
 } from '../../components/atoms';
 
 import {
@@ -38,6 +38,7 @@ import Notification from '../../components/views/Notification';
 const Sandbox = () => (
   <ThemeProvider theme={theme}>
     <Fragment>
+      <h2>Liste de Bulles </h2>
       <Notification>
         <Header>
           <Title>2 messages pour cette page</Title>
@@ -45,40 +46,33 @@ const Sandbox = () => (
             <Close />
           </ButtonClose>
         </Header>
+
+        <main>
+          <BulleExcerpt>
+            <Type>plop</Type>
+            <Title>« De nombreux clients mécontents de ce… »</Title>
+            <Contributor>Le Même en Mieux</Contributor>
+            <ButtonDelete />
+          </BulleExcerpt>
+        </main>
+
+        <Footer>
+          <Logo>Bulles</Logo>
+          <ButtonMenu>Afficher le menu</ButtonMenu>
+        </Footer>
+      </Notification>
+      
+      <h2>Liste avec une Bulle supprimée</h2>
+
+      <Notification>
         <Header>
-          <Title>
-            <Arrow />
-            &nbsp;
-            Le Même en Mieux, il y a 8 mois
-          </Title>
-          <ButtonClose>
-            <Close />
-          </ButtonClose>
-        </Header>
-        <Header>
-          <Title>Aucune de vos relations n’a créé de bulle pour cette page</Title>
+          <Title>2 messages pour cette page</Title>
           <ButtonClose>
             <Close />
           </ButtonClose>
         </Header>
 
         <main>
-          <BulleDetails>
-            <Type>plop</Type>
-            <Message>
-              De nombreux clients mécontents de 
-              Pixmania et ses vendeurs s’expriment sur les réseaux sociaux depuis 2016. 
-              Les plaintes continuent en 2017 et 2018 si l’on se réfère au forum Que Choisir.
-            </Message>
-
-            <SourceURL>http://forum.que-choisir.org/pixmania-avis-1285</SourceURL>
-
-            <Feedbacks>
-              <Approves>21</Approves>
-              <Dislikes>3</Dislikes>
-            </Feedbacks>
-          </BulleDetails>
-
           <BulleExcerpt>
             <Type>plop</Type>
             <Title>« De nombreux clients mécontents de ce… »</Title>
@@ -89,7 +83,62 @@ const Sandbox = () => (
           <BulleDeleted>
             Cette bulle ne s’affichera plus
           </BulleDeleted>
+        </main>
 
+        <Footer>
+          <Logo>Bulles</Logo>
+          <ButtonMenu>Afficher le menu</ButtonMenu>
+        </Footer>
+      </Notification>
+      
+      <h2>Détails d'une Bulle</h2>
+
+      <Notification>
+        <Header>
+          <Title>
+            <Arrow />
+            &nbsp;
+            Le Même en Mieux, il y a 8 mois
+          </Title>
+          <ButtonClose>
+            <Close />
+          </ButtonClose>
+        </Header>
+
+        <main>
+          <BulleDetails>
+            <Type>plop</Type>
+            <Message>
+              De nombreux clients mécontents de
+              Pixmania et ses vendeurs s’expriment sur les réseaux sociaux depuis 2016.
+              Les plaintes continuent en 2017 et 2018 si l’on se réfère au forum Que Choisir.
+            </Message>
+
+            <SourceURL>http://forum.que-choisir.org/pixmania-avis-1285</SourceURL>
+
+            <Feedbacks>
+              <Approves>21</Approves>
+              <Dislikes>3</Dislikes>
+            </Feedbacks>
+          </BulleDetails>
+        </main>
+
+        <Footer>
+          <Logo>Bulles</Logo>
+          <ButtonMenu>Afficher le menu</ButtonMenu>
+        </Footer>
+      </Notification>
+
+      <h2>Aucune Bulle</h2>
+      <Notification>
+        <Header>
+          <Title>Aucune de vos relations n’a créé de bulle pour cette page</Title>
+          <ButtonClose>
+            <Close />
+          </ButtonClose>
+        </Header>
+
+        <main>
           <BulleToAdd>
             Ajouter une bulle
           </BulleToAdd>
