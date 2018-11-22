1b85c8650586204b8ca47a90d4d70f6cbe36d93d
newick <erick@entre-quote.com>
2018-11-22 13:49:29 +0100

    feat: groups calls to components

---
+++ src/components/organisms/index.js
@@ -0,0 +1,4 @@
+export { default as BulleDeleted } from './BulleDeleted';
+export { default as BulleDetails } from './BulleDetails';
+export { default as BulleExcerpt } from './BulleExcerpt';
+export { default as BulleToAdd } from './BulleToAdd';
