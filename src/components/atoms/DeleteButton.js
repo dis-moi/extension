122a076bbdf20439793a7ffada1fa129642aba4c
newick <erick@entre-quote.com>
2018-11-27 16:01:01 +0100

    improved visual rendering

--- src/components/atoms/DeleteButton.js
+++ src/components/atoms/DeleteButton.js
@@ -3,5 +3,7 @@ import styled from 'styled-components';
 import DeleteIcon from './icons/Delete';
 
 export default styled.div.attrs({ children: <DeleteIcon /> })`
-    color: ${props => props.theme.otherText}
+    margin-left: auto;
+    color: ${props => props.theme.otherText};
+    
 `;
--- src/components/atoms/NotificationHeader.js
+++ src/components/atoms/NotificationHeader.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import FlexBar from './FlexBar';
 
 export default styled(FlexBar)`
-    padding-right: 2.5em;
-    padding-left: 2.0em;
+    padding-right: 25px;
+    padding-left: 22px;
     border-bottom: 1px solid rgba(188,188,201,0.20);
 `;
