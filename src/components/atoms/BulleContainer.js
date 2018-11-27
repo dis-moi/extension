5d7a836836901c726cc6a37df8cebaeedecf6b49
newick <erick@entre-quote.com>
2018-11-27 14:34:38 +0100

    fix: corrected display of many bulles

--- src/components/atoms/BulleContainer.js
+++ src/components/atoms/BulleContainer.js
@@ -7,7 +7,7 @@ export default styled.article`
     padding: 1.6em 2.5em 1.6em 2.0em;
     font-size: ${props => (props.details ? '1.5em' : '1.4em')};
 
-    article + article {
+    & + & {
         border-top: 1px solid rgba(188,188,201,0.20)
     }
 `;
