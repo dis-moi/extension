569fc009bb2a1b02e95920a874cc3d6eed00f3d8
newick <erick@entre-quote.com>
2018-11-29 15:16:08 +0100

    added all Type icons

--- src/components/atoms/icons/index.js
+++ src/components/atoms/icons/index.js
@@ -1,3 +1,16 @@
+export { default as Alternative } from './Alternative';
+export { default as Approval } from './Approval';
+export { default as Approval } from './Approval';
+export { default as ApprovalFace } from './ApprovalFace';
 export { default as Arrow } from './Arrow';
 export { default as Close } from './Close';
 export { default as Delete } from './Delete';
+export { default as NeedHelp } from './NeedHelp';
+export { default as NeedHelp } from './NeedHelp';
+export { default as Rant } from './Rant';
+export { default as Rant } from './Rant';
+export { default as Sadness } from './Sadness';
+export { default as Sadness } from './Sadness';
+export { default as Surprise } from './Surprise';
+export { default as Surprise } from './Surprise';
+export { default as Warning } from './Warning';
