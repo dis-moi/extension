d2d114bb2e739ac3df4ce31761be4d08c8450e0e
lutangar <johan.dufour@gmail.com>
2018-11-30 15:20:02 +0100

    feat: Add onClick props on AddBulle

--- src/components/molecules/AddBulle.js
+++ src/components/molecules/AddBulle.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { AddBulleLink, BulleContainer, } from '../atoms';
 import { Arrow, Bubble } from '../atoms/icons';
 
-export default () => (
+const AddBulle = ({ onClick }) => (
   <BulleContainer>
-    <AddBulleLink>
+    <AddBulleLink onClick={onClick}>
       <Bubble />
       <span>Ajouter une bulle</span>
       <Arrow />
     </AddBulleLink>
   </BulleContainer>
 );
+
+AddBulle.propTypes = {
+  onClick: PropTypes.func,
+};
+
+AddBulle.defaultProps = {
+  onClick: () => {},
+};
+
+export default AddBulle;
