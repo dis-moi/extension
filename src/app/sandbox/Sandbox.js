af3b35cfb57848e439e7b09116a911aadffaedfa
lutangar <johan.dufour@gmail.com>
2018-11-30 14:48:02 +0100

    chore: Refactor AddBulle

--- src/app/sandbox/Sandbox.js
+++ src/app/sandbox/Sandbox.js
@@ -2,13 +2,11 @@ import React, { Fragment } from 'react';
 import { ThemeProvider } from 'styled-components';
 import { hot, setConfig } from 'react-hot-loader';
 import theme from '../theme';
-import { AddBulleContainer } from '../../components/atoms';
+import { AddBulle } from '../../components/molecules';
 import { Notification, Bulle } from '../../components/organisms';
 
 const message = 'De nombreux clients mécontents de Pixmania et ses vendeurs s’expriment sur les '
     + 'réseaux sociaux depuis 2016. Les plaintes continuent en 2017 et 2018 si l’on se réfère au forum Que Choisir.';
-
-const type = 'warningFav';
 
 const Sandbox = () => (
   <ThemeProvider theme={theme}>
@@ -76,7 +74,7 @@ const Sandbox = () => (
       <Notification
         title="Aucune de vos relations n’a créé de bulle pour cette page"
       >
-        <AddBulleContainer />
+        <AddBulle />
       </Notification>
     </Fragment>
   </ThemeProvider>
--- src/components/molecules/AddBulle.js
+++ src/components/molecules/AddBulle.js
@@ -1,29 +1,24 @@
 import React from 'react';
-import styled from 'styled-components';
-import BubbleIcon from '../atoms/icons/Bubble';
-import ArrowIcon from '../atoms/icons/Arrow';
+import PropTypes from 'prop-types';
+import { AddBulleLink, BulleContainer, } from '../atoms';
+import { Arrow, Bubble } from '../atoms/icons';
 
-export const AddBulle = styled.a`
-    display: flex;
-    width: 100%;
-    font-size: 14px;
-    font-weight: bold;
-    color: ${props => props.theme.mainText};
-
-    svg + span {
-      margin-left: 10px;
-    }
-
-    span + svg {
-      margin-left: auto;
-      transform: rotate(180deg)
-    }
-`;
-
-export default () => (
-  <AddBulle>
-    <BubbleIcon />
-    <span>Ajouter une bulle</span>
-    <ArrowIcon />
-  </AddBulle>
+const AddBulle = ({ onClick }) => (
+  <BulleContainer>
+    <AddBulleLink onClick={onClick}>
+      <Bubble />
+      <span>Ajouter une bulle</span>
+      <Arrow />
+    </AddBulleLink>
+  </BulleContainer>
 );
+
+AddBulle.propTypes = {
+  onClick: PropTypes.func,
+};
+
+AddBulle.defaultProps = {
+  onClick: () => {},
+};
+
+export default AddBulle;
