4cd05a0b11eb53f9df721c9e210b3c8fb91d210d
lutangar <johan.dufour@gmail.com>
2019-01-05 11:41:55 +0100

    feat(webpack): handle views compilation via webpack
    Resolve #187

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,17 +4,17 @@ requireDir('./gulp/tasks');
 
 gulp.task('default',['build:dev']);
 gulp.task('build:dev',
-  ['webpack:build:dev', 'views:build:dev', 'copy:build:dev',
+  ['webpack:build:dev', 'copy:build:dev',
     'copy:watch', 'webpack:watch']);
 
 gulp.task('build:chromium-staging',
-  ['webpack:build:staging', 'views:build:staging', 'copy:build:staging', 'zip:build:staging']);
+  ['webpack:build:staging', 'copy:build:staging', 'zip:build:staging']);
 
 gulp.task('build:chromium-production',
-  ['webpack:build:chromium', 'views:build:chromium', 'copy:build:chromium', 'zip:build:chromium']);
+  ['webpack:build:chromium', 'copy:build:chromium', 'zip:build:chromium']);
 
 gulp.task('build:firefox-production',
-  ['webpack:build:firefox', 'views:build:firefox', 'copy:build:firefox', 'zip:build:firefox']);
+  ['webpack:build:firefox', 'copy:build:firefox', 'zip:build:firefox']);
 
 gulp.task('build:production', ['build:firefox-production', 'build:chromium-production']);
 gulp.task('build:staging', ['build:chromium-staging']);
