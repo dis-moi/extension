ba957f99248ea328a9b282fd2e0c8b728e64081e
newick <erick@entre-quote.com>
2019-01-10 15:23:30 +0100

    fix: source links have underline

--- src/components/atoms/SourceURL.js
+++ src/components/atoms/SourceURL.js
@@ -3,6 +3,11 @@ import styled from 'styled-components';
 export default styled.a`
     color: ${props => props.theme.link};
 
+    &,
+    & > span {
+        text-decoration: underline;
+    }
+
     & > span {
         vertical-align: bottom;
     }
