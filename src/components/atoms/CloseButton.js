30af423487ff6371350e5fe1a570eaaf17cc872a
newick <erick@entre-quote.com>
2019-01-10 15:14:49 +0100

    fix: changed closed icon

--- src/components/atoms/CloseButton.js
+++ src/components/atoms/CloseButton.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
 import Button from './Button';
-import CloseIcon from './icons/Delete';
+import CloseIcon from './icons/Close';
 
 export default styled(Button).attrs({ children: <CloseIcon /> })`
    margin-left: auto;
    width: 13px;
    height: 13px;
    background: none;
-   stroke: ${props => props.theme.activeColor};
+   fill: ${props => props.theme.activeColor};
 `;
 
