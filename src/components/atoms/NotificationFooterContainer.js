b22e5eaefbe72af411511142e3bc86ea6eb6bfc5
newick <erick@entre-quote.com>
2019-01-10 14:52:01 +0100

    fix(nav): same height on Chrome and Firefox

--- src/components/atoms/NotificationFooterContainer.js
+++ src/components/atoms/NotificationFooterContainer.js
@@ -1,8 +1,11 @@
 import styled from 'styled-components';
 
 export default styled.nav`
+    box-sizing: border-box;
     display: flex;
     justify-content: space-between;
+    max-height: 40px;
+    min-height: 40px;
     padding-right: 38px;
     padding-bottom: 5px;
     padding-left: 38px;
