e5c1183515381cca7c9cc3194e0e266d4b0cc55e
newick <erick@entre-quote.com>
2019-01-10 15:15:16 +0100

    fix: chanded ui Title size

--- src/components/atoms/Title.js
+++ src/components/atoms/Title.js
@@ -4,7 +4,7 @@ export default styled.p`
     display: inline-block;
     margin: 0 0 0 auto;
     padding: 1em 1em 1em 0;
-    font-size: 1.8em;
+    font-size: 1.6em;
     color: ${props => props.theme.activeColor};
     text-transform: uppercase;
     font-weight: bold;
