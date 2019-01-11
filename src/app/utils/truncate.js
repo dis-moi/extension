1ac3ee07e2c199da669f73941ec78f2a98478966
lutangar <johan.dufour@gmail.com>
2019-01-11 17:35:17 +0100

    fix(truncate): handle number of characters and preserve words options

--- src/app/utils/truncate.js
+++ src/app/utils/truncate.js
@@ -1,1 +1,17 @@
-export default (text = '', limit = 50, after = '…') => `${text.trim().split(' ').slice(0, limit).join(' ')}${after}`;
+export const truncateButPreserveWords = (text = '', limit = 50, after = '…') => {
+  const words = text.trim().split(' ');
+
+  while (words.join(' ').length > limit) {
+    words.pop();
+  }
+
+  return `${words.join(' ')}${after}`;
+};
+
+export const simpleTruncate = (text = '', limit = 50, after = '…') => `${text.trim().substring(0, limit)}${after}`;
+
+export default (text = '', limit = 50, preserveWords = true, after = '…') => (
+  preserveWords
+    ? truncateButPreserveWords(text, limit - after.length, after)
+    : simpleTruncate(text, limit - after.length, after)
+);
