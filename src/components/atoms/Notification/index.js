f54467997f742e6ea9cb74e358cdb6bfcc831e3a
lutangar <johan.dufour@gmail.com>
2019-01-14 11:50:04 +0100

    refactor(title): `ListNoticesTitle` is now a generic `NotificationContentTitle`

--- src/components/atoms/Notification/index.js
+++ src/components/atoms/Notification/index.js
@@ -1,4 +1,5 @@
 export { default as Container } from './Container';
+export { default as ContentTitle } from './ContentTitle';
 export { default as FooterContainer } from './FooterContainer';
 export { default as HeaderContainer } from './HeaderContainer';
 export { default as Main } from './Main';
--- src/components/screens/Notice/List.js
+++ src/components/screens/Notice/List.js
@@ -1,33 +1,35 @@
 import React from 'react';
+import { NotificationContentTitle } from '../../atoms';
+import { AddNotice} from '../../molecules';
 import { Notification, Notice} from '../../organisms';
-import {AddNotice} from '../../molecules';
-import { ListNoticesTitle } from '../../atoms';
 
 const message = 'De nombreux clients mécontents de Pixmania et ses vendeurs s’expriment sur les '
     + 'réseaux sociaux depuis 2016. Les plaintes continuent en 2017 et 2018 si l’on se réfère au forum Que Choisir.';
 
-export default ({ match }) => (
-  <Notification notices>
-    <ListNoticesTitle>Notices pour cette page</ListNoticesTitle>
-    <Notice
-      match={match}
-      type="Other"
-      message={message}
-      contributor="Le Même en Mieux"
-      source="http://forum.que-choisir.org/pixmania-avis-1285"
-      approves={21}
-      dislikes={3}
-    />
-    <Notice
-      match={match}
-      type="Rant"
-      message={message}
-      contributor="Le Même en Mieux"
-      source="http://forum.que-choisir.org/pixmania-avis-1285"
-      approves={21}
-      dislikes={3}
-      deleted
-    />
-    <AddNotice />
-  </Notification>
-);
+export default ({ match }) => {
+  return (
+    <Notification notices>
+      <NotificationContentTitle>Notices pour cette page</NotificationContentTitle>
+      <Notice
+        match={match}
+        type="Other"
+        message={message}
+        contributor="Le Même en Mieux"
+        source="http://forum.que-choisir.org/pixmania-avis-1285"
+        approves={21}
+        dislikes={3}
+      />
+      <Notice
+        match={match}
+        type="Rant"
+        message={message}
+        contributor="Le Même en Mieux"
+        source="http://forum.que-choisir.org/pixmania-avis-1285"
+        approves={21}
+        dislikes={3}
+        deleted
+      />
+      <AddNotice />
+    </Notification>
+  );
+};
