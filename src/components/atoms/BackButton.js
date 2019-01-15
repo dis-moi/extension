9ee6a55c393a06144b07e16b41dd1f7435ad0285
newick <erick@entre-quote.com>
2019-01-15 15:57:44 +0100

    fix(buttons): removed useless background property

--- src/components/atoms/BackButton.js
+++ src/components/atoms/BackButton.js
@@ -6,7 +6,6 @@ import ArrowIcon from './icons/Arrow';
 export default styled(Button).attrs({ children: <ArrowIcon /> })`
     height: 13px;
     padding-right: 10px;
-    background: none;
     stroke: ${props => props.theme.activeColor};
 `;
 
