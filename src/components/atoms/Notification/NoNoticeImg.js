4a3769bede6e7bbd5323476c8b2f18c867830c46
newick <erick@entre-quote.com>
2019-01-15 16:34:59 +0100

    feat(NoNotice screen): added a screen in case of no displayed notice

---
+++ src/components/atoms/Notification/NoNoticeImg.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export default styled.div`
+    margin-top: 28px;
+    text-align: center;
+
+    & > svg {
+        width: 103px;
+        height: 97px;
+    }
+`;
