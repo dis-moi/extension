0aedb413f909fb96275a1629ca52b013b7ddac31
newick <erick@entre-quote.com>
2019-01-18 12:28:52 +0100

    fix: margins

--- src/components/atoms/Notification/HeaderContainer.js
+++ src/components/atoms/Notification/HeaderContainer.js
@@ -1,10 +1,12 @@
 import styled from 'styled-components';
 
-export default styled.div`
+export default styled.div`  
+    box-sizing: border-box;
     display: flex;
     justify-content: center;
     align-items: center;
-    height: 32px;
+    min-height: 32px;
+    max-height: 32px;
     padding-right: 10px;
     padding-left: 12px;
     border-bottom: 1px solid #A6B1C0;
