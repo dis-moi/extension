4f95407b674ba602c5fcd81b9ee8ed2b626b4947
newick <erick@entre-quote.com>
2019-01-22 15:32:04 +0100

    feat(loader): display loading screen in privacy screen

--- src/components/screens/Account/index.js
+++ src/components/screens/Account/index.js
@@ -3,10 +3,12 @@ import React from 'react';
 import Filters from './Filters';
 import Menu from './Menu';
 import Error from '../Error';
+import Loading from '../Loading';
 
 export default ({ match }) => (
   <Switch>
     <Route path={match.url} exact component={Menu} />
+    <Route path={`${match.url}/privacy`} component={Loading} />
     <Route path={`${match.url}/filters`} component={Filters} />
     <Route component={Error} />
   </Switch>
