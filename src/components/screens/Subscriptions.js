2575ec701466ee61195dc0c9289b3bede29fee8e
newick <erick@entre-quote.com>
2019-01-22 14:34:46 +0100

    feat(subscriptions screen): added content

--- src/components/screens/Subscriptions.js
+++ src/components/screens/Subscriptions.js
@@ -1,8 +1,34 @@
 import React from 'react';
-import { Notification} from '../organisms';
+import { Notification } from '../organisms';
+import Subscriptions from '../atoms/icons/illustrations/Subscriptions';
+import { Container, SubscriptionsData } from '../atoms/Subscriptions';
+import { BorderButton, CenterContainer } from '../atoms';
 
 export default () => (
   <Notification title="Abonnements">
-        Abonnements
+    <Container>
+      <Subscriptions />
+
+      <SubscriptionsData>
+        <div>
+          <span>5</span>
+          <span>Abonnements</span>
+        </div>
+        <div>
+          <span>3</span>
+          <span>
+            Notices
+            <br />
+            publiées
+          </span>
+        </div>
+        <div>
+          <span>155</span>
+          <span>Vues</span>
+        </div>
+      </SubscriptionsData>
+
+      <BorderButton>Gérer</BorderButton>
+    </Container>
   </Notification>
 );
