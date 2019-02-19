8a625ee04402223eb4d1455d0ee2a757fe7cc875
newick <erick@entre-quote.com>
2019-02-19 12:42:50 +0100

    corrected Message render

--- src/components/atoms/Message.js
+++ src/components/atoms/Message.js
@@ -1,24 +1,10 @@
-import React from 'react';
-import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import { sanitize } from 'dompurify';
 
-export const MessageContainer = styled.div`
-    p {
-        margin: 6px 0 12px;
-        font-size: 15px;
-        line-height: 1.4;
-        color: ${props => props.theme.primaryColor}
-    }
+export const Message = styled.p`
+  margin: 6px 0 12px;
+  font-size: 15px;
+  line-height: 1.4;
+  color: ${props => props.theme.primaryColor};
 `;
-
-// eslint-disable-next-line react/no-danger
-export const Message = ({ children }) => (
-  <MessageContainer dangerouslySetInnerHTML={{ __html: sanitize(children) }} />
-);
-
-Message.propTypes = {
-  children: PropTypes.string.isRequired,
-};
 
 export default Message;
