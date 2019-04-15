a3fa5ee63b21bb31d3840c2f8fb07723f2e9aa0d
newick <erick@entre-quote.com>
2019-04-15 14:34:39 +0200

    style: change icon to delete Notice

--- src/components/organisms/Notice/DeleteButton.js
+++ src/components/organisms/Notice/DeleteButton.js
@@ -7,20 +7,7 @@ export default styled(Button).attrs({ children: <DeleteIcon /> })`
   box-sizing: border-box;
   display: flex;
   align-items: center;
-  width: 17px;
-  height: 17px;
-  padding: 0 4px;
+  width: 18px;
+  height: 23px;
   margin-left: 16px;
-  background: ${props => props.theme.navInactive};
-  border-radius: 50%;
-
-  & > svg {
-      fill: #fff;
-      width: 9px;
-      height: 9px;
-  }
-
-  &:hover {
-    background-color: ${props => props.theme.formError}
-  }
 `;
