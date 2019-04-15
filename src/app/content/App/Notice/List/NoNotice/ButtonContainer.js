9c3ea029bc61f5aadc46d1c716edbeb7103d6e83
newick <erick@entre-quote.com>
2019-04-15 15:23:24 +0200

    style: edit NoNotice display

---
+++ src/app/content/App/Notice/List/NoNotice/ButtonContainer.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export default styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 93px;
+
+  & > button {
+    margin-top: -10px;
+    margin-left: 16px;
+  }
+`;
