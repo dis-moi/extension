9013f953fff3c99ffabced317983dc3f254b6302
lutangar <johan.dufour@gmail.com>
2019-04-16 11:07:21 +0200

    feat(tab): add `tabId` state slice to get `tabId` from content context

--- src/app/content/reducers/index.ts
+++ src/app/content/reducers/index.ts
@@ -5,11 +5,13 @@ import { MemoryHistory } from 'history';
 import installationDetails from './installationDetails';
 import open from './open';
 import recommendations from './recommendations';
+import tabId from './tabId';
 
 export default (history: MemoryHistory) =>
   combineReducers({
     installationDetails,
     open,
     recommendations,
+    tabId,
     router: connectRouter(history)
   });
