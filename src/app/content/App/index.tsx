dbe5ca9ef7a44884d111653e39a8981b680bb19c
lutangar <johan.dufour@gmail.com>
2019-04-24 17:55:23 +0200

    chore(Content): create dedicated file for "content" component

--- src/app/content/App/index.tsx
+++ src/app/content/App/index.tsx
@@ -1,48 +1,12 @@
 import React from 'react';
-import { Provider, connect } from 'react-redux';
-import { Redirect, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'connected-react-router/immutable';
 import { ThemeProvider, StyleSheetManager } from 'styled-components';
 import theme from '../../theme';
-import store, { history, State } from '../store';
-import GlobalStyle from '../GlobalStyle';
-import Account from './Account';
-import Error from './Error';
-import Loading from './Loading';
-import Notice from './Notice';
-import Subscriptions from './Subscriptions';
-import Help from './Help';
-import { isOpen } from '../selectors';
+import store, { history } from '../store';
+import Content from './Content';
 
 const DELAY_BEFORE_SHOWING = process.env.NODE_ENV === 'production' ? 4000 : 10;
-
-const mapStateToProps = (state: State) => ({
-  open: isOpen(state)
-});
-
-interface ContentProps {
-  loaded: boolean;
-  open: boolean;
-}
-const Content = connect(mapStateToProps)(({ loaded, open }: ContentProps) =>
-  open ? (
-    <React.Fragment>
-      <GlobalStyle />
-      {loaded ? (
-        <Switch>
-          <Redirect exact path="/" to="/notices" />
-          <Route path="/notices" component={Notice} />
-          <Route path="/subscriptions" component={Subscriptions} />
-          <Route path="/help" component={Help} />
-          <Route path="/account" component={Account} />
-          <Route component={Error} />
-        </Switch>
-      ) : (
-        <Loading />
-      )}
-    </React.Fragment>
-  ) : null
-);
 
 interface AppProps {
   contentDocument: Document;
