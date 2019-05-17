46148fcbb280fd102d3b6908ff299a4d01a063c5
Jalil Arfaoui <jalil@arfaoui.net>
2019-05-17 12:04:35 +0200

    style: remove unused export const

--- src/app/actions/updateDraftNotices.ts
+++ src/app/actions/updateDraftNotices.ts
@@ -4,8 +4,10 @@ interface UpdateDraftNoticesAction extends BaseAction {
   type: 'UPDATE_DRAFT_NOTICES';
   payload: { drafts: {} };
 }
-export const updateDraftNotices = (drafts: {}): UpdateDraftNoticesAction => ({
+
+const updateDraftNotices = (drafts: {}): UpdateDraftNoticesAction => ({
   type: 'UPDATE_DRAFT_NOTICES',
   payload: { drafts }
 });
+
 export default updateDraftNotices;
