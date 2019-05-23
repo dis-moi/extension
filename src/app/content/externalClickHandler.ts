9e8a112793c39298a371b66453bf8802e793626b
lutangar <johan.dufour@gmail.com>
2019-05-23 17:36:33 +0200

    fix(external-click-handler): don't close the UI when the user click on an interactive Html element

--- src/app/content/externalClickHandler.ts
+++ src/app/content/externalClickHandler.ts
@@ -2,10 +2,35 @@ import { Store } from 'redux';
 import { close } from 'app/actions/ui';
 import { isOpen } from './selectors';
 
-export default (store: Store) => () => {
+export const interactiveElementsSelectors: string[] = [
+  'a',
+  'details',
+  'dialog',
+  'menu',
+  'menuitem',
+  'summary',
+  'button',
+  'datalist',
+  'input',
+  'label',
+  'meter',
+  'optgroup',
+  'option',
+  'select',
+  'textarea',
+  '[onclick]'
+];
+
+export const isHtmlElementInteractive = (element: HTMLElement) =>
+  interactiveElementsSelectors.some(selector => element.matches(selector));
+
+export default (store: Store) => (e: MouseEvent) => {
   const state = store.getState();
 
-  if (isOpen(state)) {
+  const element = e.target as HTMLElement;
+  const interactive = isHtmlElementInteractive(element);
+
+  if (!interactive && isOpen(state)) {
     store.dispatch(close());
   }
 };
---
+++ test/app/content/externalClickHandler.test.ts
@@ -0,0 +1,21 @@
+import chai from 'chai';
+import { JSDOM } from 'jsdom';
+import { isHtmlElementInteractive } from '../../../src/app/content/externalClickHandler';
+
+const expect = chai.expect;
+
+const dom = new JSDOM();
+const document = dom.window.document;
+
+describe('isHtmlElementInteractive', () => {
+  it('can tell if actual Html elements are interactive', () => {
+    expect(isHtmlElementInteractive(document.createElement('a'))).to.be.true;
+    expect(isHtmlElementInteractive(document.createElement('button'))).to.be
+      .true;
+
+    const element = document.createElement('div');
+    element.setAttribute('onClick', '() => {}');
+
+    expect(isHtmlElementInteractive(element)).to.be.true;
+  });
+});
