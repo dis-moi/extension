6e68f3578e77558b67769fbae184ed2ec9e89303
lutangar <johan.dufour@gmail.com>
2019-05-24 11:05:15 +0200

    chore(env): check if ONBOARDING_ORIGIN is defined before using it

--- src/app/background/index.ts
+++ src/app/background/index.ts
@@ -19,13 +19,21 @@ configureSentryScope(scope => {
   scope.setTag('context', 'background');
 });
 
-if (process.env.NODE_ENV !== 'production') {
-  console.info('NODE_ENV', process.env.NODE_ENV);
+const {
+  NODE_ENV,
+  UNINSTALL_ORIGIN,
+  HEAP_APPID,
+  ONBOARDING_ORIGIN,
+  REFRESH_MC_INTERVAL
+} = process.env;
+
+if (NODE_ENV !== 'production') {
+  console.info('NODE_ENV', NODE_ENV);
 }
 console.info(`LMEM_BACKEND_ORIGIN "${LMEM_BACKEND_ORIGIN}"`);
 
 const setUninstallURL = (heapUserId: string) => {
-  const uninstallOrigin = process.env.UNINSTALL_ORIGIN;
+  const uninstallOrigin = UNINSTALL_ORIGIN;
   if (typeof uninstallOrigin === 'string') {
     chrome.runtime.setUninstallURL(
       uninstallOrigin + '?u=' + encodeURIComponent(heapUserId || '-1')
@@ -33,7 +41,7 @@ const setUninstallURL = (heapUserId: string) => {
   }
 };
 
-const heapAppId = process.env.HEAP_APPID;
+const heapAppId = HEAP_APPID;
 if (typeof heapAppId === 'string') {
   console.info(`Heap loading with appId "${heapAppId}"`);
   loadHeap(heapAppId)
@@ -65,15 +73,21 @@ fetchContentScript('/js/grabDraftNotices.js').then(contentCode =>
   )
 );
 
-const state: BackgroundState = store.getState();
-if (!getInstallationDetails(state).datetime) {
-  onInstalled(process.env.ONBOARDING_ORIGIN)(store.dispatch);
+if (ONBOARDING_ORIGIN) {
+  const state: BackgroundState = store.getState();
+  if (!getInstallationDetails(state).datetime) {
+    onInstalled(ONBOARDING_ORIGIN)(store.dispatch);
+  }
+} else {
+  console.warn(
+    'No installation details: assuming "process.env.ONBOARDING_ORIGIN" is deliberately not defined.'
+  );
 }
 
 dispatchInitialStateFromBackend()(store.dispatch);
 // store initialization from the kraft server
 
-const refreshInterval = Number(process.env.REFRESH_MC_INTERVAL);
+const refreshInterval = Number(REFRESH_MC_INTERVAL);
 if (refreshInterval > 0) {
   console.info(
     `Matching contexts will be refreshed every ${refreshInterval /
