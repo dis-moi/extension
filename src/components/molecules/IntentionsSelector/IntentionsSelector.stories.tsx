570781b37ca47f2dd4578b6722ec360eb98fb673
newick <erick@entre-quote.com>
2019-06-10 15:17:15 +0200

    style(Intention selector); added all intentions stories

--- src/components/molecules/IntentionsSelector/IntentionsSelector.stories.tsx
+++ src/components/molecules/IntentionsSelector/IntentionsSelector.stories.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import IntentionsSelector from './IntentionsSelector';
 
-storiesOf('organisms/IntentionsSelector', module).add('normal', () => (
-  <IntentionsSelector value="disapproval" />
-));
+storiesOf('organisms/IntentionsSelector', module)
+  .add('approval', () => <IntentionsSelector value="approval" />)
+  .add('disapproval', () => <IntentionsSelector value="disapproval" />)
+  .add('information', () => <IntentionsSelector value="information" />)
+  .add('alternative', () => <IntentionsSelector value="alternative" />)
+  .add('other', () => <IntentionsSelector value="other" />);
