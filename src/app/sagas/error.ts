45163ed310ccc988e83a5004e2b07c7f83ea24bb
lutangar <johan.dufour@gmail.com>
2019-06-14 15:56:27 +0200

    feat(error-handling): capture saga errors with Sentry if enabled

--- src/app/sagas/error.ts
+++ src/app/sagas/error.ts
@@ -1,10 +1,16 @@
 import { takeLatest } from 'redux-saga/effects';
+import { SagaIterator } from 'redux-saga';
+import { captureException } from '@sentry/browser';
 import { BaseAction, ErrorAction } from '../actions';
 
 export function* handleErrorSaga({
   payload: e
-}: ErrorAction): IterableIterator<any> {
-  console.error(e);
+}: ErrorAction): SagaIterator {
+  if (process.env.SENTRY_ENABLE) {
+    captureException(e);
+  } else {
+    console.error(e);
+  }
 }
 
 export default function* errorRootSaga() {
