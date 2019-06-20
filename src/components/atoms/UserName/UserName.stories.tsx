d3b38f0fab9a3e796e6a297a74fe782a79638108
newick <erick@entre-quote.com>
2019-06-20 11:43:08 +0200

    feat(SubscribeToContributors): added user name atom

---
+++ src/components/atoms/UserName/UserName.stories.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { withKnobs, text } from '@storybook/addon-knobs';
+import UserName from './UserName';
+import Faker from 'faker';
+
+const defaultUserName = Faker.name.findName();
+
+storiesOf('atoms/UserName', module)
+  .addDecorator(withKnobs)
+  .add('normal', () => <UserName>{text('content', defaultUserName)}</UserName>);
