438d25000912bb92087305c0791139f4eac8a6b2
lutangar <johan.dufour@gmail.com>
2019-06-24 16:50:29 +0200

    feat(form): add Fields adapter

---
+++ src/components/atoms/Button.ts
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+import * as React from 'react';
+import { Theme } from '../../app/theme';
+
+export interface ButtonProps {
+  as?: string | React.ComponentType<any>;
+  theme?: Theme;
+  loading?: boolean;
+}
+
+export default styled.button<ButtonProps>`
+  box-sizing: border-box;
+  padding: 0;
+  font-weight: bold;
+  line-height: 1;
+  color: ${props => props.theme.secondaryColor};
+  text-transform: uppercase;
+  text-decoration: underline;
+  background: none;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+
+  &:hover {
+    color: ${props => props.theme.activeColor};
+  }
+
+  &:focus {
+    outline: 1px dotted ${props => props.theme.activeColor};
+  }
+`;
