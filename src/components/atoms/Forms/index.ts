ee2a8ea1a2b9956d01bb8f097479e4d957d57f1c
lutangar <johan.dufour@gmail.com>
2019-06-24 16:50:29 +0200

    feat(form): add Fields adapter

--- src/components/atoms/Forms/index.ts
+++ src/components/atoms/Forms/index.ts
@@ -1,1 +1,4 @@
+export { default as Container } from './Container';
+export { default as Input } from './Input/Input';
+export { default as Error } from './Error/Error';
 export { default as Textarea } from './Textarea/Textarea';
