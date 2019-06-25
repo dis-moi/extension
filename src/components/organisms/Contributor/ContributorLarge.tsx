5a7c59c53274d469e9fa9a4f53b9590b8da5a435
newick <erick@entre-quote.com>
2019-06-25 23:18:57 +0200

    style(Contributor): separated Contributor content from the card

--- src/components/organisms/Contributor/ContributorLarge.tsx
+++ src/components/organisms/Contributor/ContributorLarge.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import styled, { css } from 'styled-components';
-
+import styled from 'styled-components';
 import Button from 'components/atoms/Button';
 import Avatar from 'components/atoms/Avatar/Avatar';
 import AvatarDefault from 'components/atoms/icons/AvatarDefault';
