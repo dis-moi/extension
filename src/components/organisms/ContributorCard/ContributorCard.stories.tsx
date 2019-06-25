3f7a195778f2924a48e84a3a2d49ba64498fac5e
newick <erick@entre-quote.com>
2019-06-25 11:25:12 +0200

    style(Contributor Card): created case of empty contributor message

--- src/components/organisms/ContributorCard/ContributorCard.stories.tsx
+++ src/components/organisms/ContributorCard/ContributorCard.stories.tsx
@@ -2,6 +2,6 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import ContributorCard from './ContributorCard';
 
-storiesOf('organisms/ContributorCard', module).add('default', () => (
-  <ContributorCard />
-));
+storiesOf('organisms/ContributorCard', module)
+  .add('default', () => <ContributorCard />)
+  .add('empty', () => <ContributorCard />);
