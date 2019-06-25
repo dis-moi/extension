343570d3f9725c1e2ef6060d48f9ffd0e54125e0
newick <erick@entre-quote.com>
2019-06-25 10:58:26 +0200

    style(SubscribeToContributors): added Contributor Card organisms

---
+++ src/components/atoms/Stat/StatType.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export default styled.span`
+  font-size: 10px;
+  text-transform: uppercase;
+`;
---
+++ src/components/organisms/ContributorCard/ContributorCard.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import Avatar from 'components/atoms/Avatar/Avatar';
+import UserName from 'components/atoms/UserName/UserName';
+import Stat from 'components/atoms/Stat/Stat';
+import StatType from 'components/atoms/Stat/StatType';
+import Container from './Container';
+import ContributorInfosWrapper from './ContributorInfosWrapper';
+import ContributorInfos from './ContributorInfos';
+import ContributorMessage from './ContributorMessage';
+import StatsWrapper from './StatsWrapper';
+import ContributorButton from './ContributorButton';
+
+export const ContributorNav = () => (
+  <Container>
+    <ContributorInfosWrapper>
+      <Avatar />
+
+      <ContributorInfos>
+        <UserName>Jean Michel</UserName>
+        <StatsWrapper>
+          <Stat>
+            120 <StatType>Bulles</StatType>
+          </Stat>
+          <Stat>
+            120 <StatType>J'aime</StatType>
+          </Stat>
+          <Stat>
+            120 <StatType>Abonnés</StatType>
+          </Stat>
+        </StatsWrapper>
+        <ContributorButton>Abonné</ContributorButton>
+      </ContributorInfos>
+    </ContributorInfosWrapper>
+
+    <ContributorMessage>
+      Passionné d’infos et s’intox, je vous préviens parfois (mais pas toujours
+      ^^) quand vous êtes en train de consulter un hoax, c’est-à-dire un message
+      poignant, révoltant ou alarmant… mais faux ! Mes sources : Hoaxbuster,
+      HoaxKiller, Hoaxteam, Secuser.com.
+    </ContributorMessage>
+  </Container>
+);
+
+export default ContributorNav;
