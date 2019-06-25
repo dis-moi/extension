e7a39da935c57224fcb9c48cf21a24cc34bb4c88
newick <erick@entre-quote.com>
2019-06-25 11:25:12 +0200

    style(Contributor Card): created case of empty contributor message

---
+++ src/components/organisms/ContributorCard/ContributorEmptyMessage.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export default styled.p`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  min-height: 135px;
+  padding-right: 30px;
+  padding-left: 30px;
+  font-size: 15px;
+`;
