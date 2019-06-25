a44e6e3a2a48666244ecc6157080fa5d812cef5a
newick <erick@entre-quote.com>
2019-06-25 10:58:26 +0200

    style(SubscribeToContributors): added Contributor Card organisms

--- src/components/organisms/ContributorCard/ContributorCard.stories.tsx
+++ src/components/organisms/ContributorCard/ContributorCard.stories.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import { MemoryRouter as Router } from 'react-router-dom';
 import { storiesOf } from '@storybook/react';
 import ContributorCard from './ContributorCard';
 
-storiesOf('organisms/ContributorCard', module)
-  .add('default', () => <ContributorCard />)
-  .add('empty', () => <ContributorCard />);
+storiesOf('organisms/ContributorCard', module).add('default', () => (
+  <ContributorCard />
+));
