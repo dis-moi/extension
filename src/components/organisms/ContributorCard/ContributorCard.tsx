f4cccf24cc202144de1d82717f98376f189a7f49
newick <erick@entre-quote.com>
2019-06-25 21:26:40 +0200

    style(Contributor Card): changed Contributor Description code

--- src/components/organisms/ContributorCard/ContributorCard.tsx
+++ src/components/organisms/ContributorCard/ContributorCard.tsx
@@ -6,7 +6,7 @@ import StatType from 'components/atoms/Stat/StatType';
 import Container from './Container';
 import ContributorInfosWrapper from './ContributorInfosWrapper';
 import ContributorInfos from './ContributorInfos';
-import ContributorMessage from './ContributorMessage';
+import ContributorDescription from './ContributorDescription';
 import StatsWrapper from './StatsWrapper';
 import ContributorButton from './ContributorButton';
 
@@ -17,6 +17,7 @@ export const ContributorNav = () => (
 
       <ContributorInfos>
         <UserName>Jean Michel</UserName>
+
         <StatsWrapper>
           <Stat>
             120 <StatType>Bulles</StatType>
@@ -28,16 +29,17 @@ export const ContributorNav = () => (
             120 <StatType>Abonnés</StatType>
           </Stat>
         </StatsWrapper>
+
         <ContributorButton>Abonné</ContributorButton>
       </ContributorInfos>
     </ContributorInfosWrapper>
 
-    <ContributorMessage>
+    <ContributorDescription>
       Passionné d’infos et s’intox, je vous préviens parfois (mais pas toujours
       ^^) quand vous êtes en train de consulter un hoax, c’est-à-dire un message
       poignant, révoltant ou alarmant… mais faux ! Mes sources : Hoaxbuster,
       HoaxKiller, Hoaxteam, Secuser.com.
-    </ContributorMessage>
+    </ContributorDescription>
   </Container>
 );
 
