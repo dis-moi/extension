482281e3bc39f37832c05627232de7b6b26a7fe2
newick <erick@entre-quote.com>
2019-06-25 11:25:12 +0200

    style(Contributor Card): created case of empty contributor message

--- src/components/organisms/ContributorCard/ContributorCard.stories.tsx
+++ src/components/organisms/ContributorCard/ContributorCard.stories.tsx
@@ -3,6 +3,6 @@ import { MemoryRouter as Router } from 'react-router-dom';
 import { storiesOf } from '@storybook/react';
 import ContributorCard from './ContributorCard';
 
-storiesOf('organisms/ContributorCard', module).add('default', () => (
-  <ContributorCard />
-));
+storiesOf('organisms/ContributorCard', module)
+  .add('default', () => <ContributorCard />)
+  .add('empty', () => <ContributorCard />);
