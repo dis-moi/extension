dabdc62fe6ff6c4fa669e37989d3eb9e7d7134ef
Jalil Arfaoui <jalil@arfaoui.net>
2019-07-03 16:52:41 +0200

    feat: add settings entrypoint

--- webpack/config.entry.js
+++ webpack/config.entry.js
@@ -3,9 +3,8 @@ const path = require('path');
 module.exports = (env, srcPath) => ({
   background: ['@babel/polyfill', path.join(srcPath, './app/background/')],
   content: ['@babel/polyfill', path.join(srcPath, './app/content/')],
-  ...(
-    env.build === 'dev' || env.build === 'staging'
-      ? {test: [path.join(__dirname, '../test/integration/')]}
-      : {}
-  )
+  settings: ['@babel/polyfill', path.join(srcPath, './app/settings/')],
+  ...(env.build === 'dev' || env.build === 'staging'
+    ? { test: [path.join(__dirname, '../test/integration/')] }
+    : {})
 });
