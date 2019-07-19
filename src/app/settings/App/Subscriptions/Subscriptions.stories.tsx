f1e1b1f292a41dfd5fee907f726fa22394a8d6c5
newick <erick@entre-quote.com>
2019-07-19 15:37:13 +0200

    feat: created SubscriptionsScreen

---
+++ src/app/settings/App/Subscriptions/Subscriptions.stories.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import Subscriptions from './Subscriptions';
+
+storiesOf('screens/SubscriptionsScreen', module).add('large', () => (
+  <Subscriptions />
+));
