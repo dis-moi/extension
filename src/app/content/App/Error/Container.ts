0dbd991f15bef16b1897eba70f480d6106b9fa9a
newick <erick@entre-quote.com>
2019-07-26 13:52:26 +0200

    fix(Error screen): center button

--- src/app/content/App/Error/Container.ts
+++ src/app/content/App/Error/Container.ts
@@ -1,6 +1,11 @@
 import styled from 'styled-components';
+import BorderButton from 'components/atoms/Button/BorderButton/BorderButton';
 
 export default styled.article`
   padding: 29px 24px 26px;
   text-align: center;
+
+  ${BorderButton} {
+    margin: 0 auto;
+  }
 `;
