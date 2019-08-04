a7d2426e36711edf36dc2bcc53a54299f3feb05e
lutangar <johan.dufour@gmail.com>
2019-08-04 20:09:08 +0200

    feat(contribution-form): show form errors when a field is touched and invalid

--- src/components/atoms/Button/AddNoticeButton/AddNoticeButton.stories.tsx
+++ src/components/atoms/Button/AddNoticeButton/AddNoticeButton.stories.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import { MemoryRouter as Router } from 'react-router';
 import { storiesOf } from '@storybook/react';
 import AddNoticeButton from './AddNoticeButton';
 
-storiesOf('atoms/Buttons/AddNoticeButton', module).add('normal', () => (
-  <AddNoticeButton />
-));
+storiesOf('atoms/Buttons/AddNoticeButton', module)
+  .addDecorator(getStory => <Router>{getStory()}</Router>)
+  .add('normal', () => <AddNoticeButton />);
