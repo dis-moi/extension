5c91420770d53f074941e3801cb63b59262ae187
Benjamin Menant <bmenant@lmem.net>
2019-08-09 19:37:37 +0200

    fix(unfoldRating): add unfoldRating

--- src/app/content/sagas/notices.ts
+++ src/app/content/sagas/notices.ts
@@ -1,6 +1,7 @@
-import { put, takeLatest, select } from 'redux-saga/effects';
+import { put, takeLatest, select, all } from 'redux-saga/effects';
 import { getNotices, getTabId } from '../selectors';
-import { noticesUpdated } from '../../actions/notices';
+import { noticesUpdated, unfoldNotice } from '../../actions/notices';
+import { Notice } from '../../lmem/notice';
 
 export function* updateNoticesSaga() {
   const notices = yield select(getNotices);
@@ -8,6 +9,12 @@ export function* updateNoticesSaga() {
   yield put(noticesUpdated(notices, { tab, sendToBackground: true }));
 }
 
+export function* rateUnfoldedNoticesSaga() {
+  const notices = yield select(getNotices);
+  yield all(notices.map(({ id }: Notice) => put(unfoldNotice(id))));
+}
+
 export default function* noticesRootSaga() {
   yield takeLatest(['READ_NOTICE', 'FEEDBACK_ON_NOTICE'], updateNoticesSaga);
+  yield takeLatest('READ_NOTICE', rateUnfoldedNoticesSaga);
 }
