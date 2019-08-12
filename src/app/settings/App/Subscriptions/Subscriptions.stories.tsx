8df88d6bb4bda115950d48f974af245ff4709028
newick <erick@entre-quote.com>
2019-08-12 16:44:42 +0200

    style(SubscriptionsEmpty): added view

--- src/app/settings/App/Subscriptions/Subscriptions.stories.tsx
+++ src/app/settings/App/Subscriptions/Subscriptions.stories.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import Subscriptions from './Subscriptions';
+import SubscriptionsEmpty from './SubscriptionsEmpty';
 import Suggestions from './Suggestions';
 import Wrapper from './Wrapper';
 
 storiesOf('screens/SubscriptionsScreen', module)
   .add('subcriptions', () => <Subscriptions />)
+  .add('subcriptions empty', () => <SubscriptionsEmpty />)
   .add('suggestions', () => <Suggestions />);
