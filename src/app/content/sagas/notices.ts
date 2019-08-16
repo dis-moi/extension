38893fa950d3ff81660c9b17b5045eab5d810d6e
Benjamin Menant <bmenant@lmem.net>
2019-08-17 00:03:34 +0200

    fix(markRead): mark read when ui is closed

--- src/app/content/sagas/notices.ts
+++ src/app/content/sagas/notices.ts
@@ -1,7 +1,13 @@
-import { put, takeLatest, select } from 'redux-saga/effects';
+import { put, takeLatest, select, all } from 'redux-saga/effects';
 import { getNotices, getTab, hasNoticesToDisplay } from '../selectors';
-import { noticesUpdated, updateNoticesFailed } from '../../actions/notices';
+import {
+  noticesUpdated,
+  updateNoticesFailed,
+  markNoticeRead
+} from 'app/actions/notices';
 import { close } from '../../actions/ui';
+import { CLOSED } from 'app/constants/ActionTypes';
+import { StatefulNotice } from 'app/lmem/notice';
 
 export function* updateNoticesSaga() {
   try {
@@ -18,6 +24,19 @@ export function* updateNoticesSaga() {
   }
 }
 
+export function* markNoticesReadSaga() {
+  // FIXME find out if we need to guard some cases so we don’t accidentally mark read notices on some close cases.
+  const notices = yield select(getNotices);
+  yield all(notices.map(({ id }: StatefulNotice) => put(markNoticeRead(id))));
+}
+
 export default function* noticesRootSaga() {
-  yield takeLatest(['READ_NOTICE', 'FEEDBACK_ON_NOTICE'], updateNoticesSaga);
+  yield all([
+    // FIXME change all strings to constants because it’s a pain the ass to refactor (i.e. rename)
+    yield takeLatest(
+      ['MARK_NOTICE_READ', 'FEEDBACK_ON_NOTICE'],
+      updateNoticesSaga
+    ),
+    yield takeLatest([CLOSED], markNoticesReadSaga)
+  ]);
 }
