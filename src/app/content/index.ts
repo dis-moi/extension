7f643d5cbc5eb4cf816dfb6604f0e15c386710d1
lutangar <johan.dufour@gmail.com>
2019-08-22 11:46:10 +0200

    feat(sentry): do not track host page error
    fix(sentry): change import for require

--- src/app/content/index.ts
+++ src/app/content/index.ts
@@ -2,31 +2,40 @@
 import { Scope } from '@sentry/browser';
 
 if (!(window as CustomWindow).__LMEM__CONTENT_SCRIPT_INJECTED__) {
-  const { configureSentryScope, initSentry } = require('../utils/sentry');
+  const {
+    captureException,
+    configureSentryScope,
+    initSentry
+  } = require('../utils/sentry');
 
   try {
-    initSentry();
-  } catch (error) {
-    console.warn('Could not init Sentry in contentScript', error);
-  }
-  configureSentryScope((scope: Scope) => {
-    scope.setTag('context', 'content');
-  });
+    try {
+      initSentry();
 
-  require('typeface-lato');
-  require('typeface-sedgwick-ave');
-  const store = require('./store').default;
-  const documentReady = require('../utils/documentReady').default;
-  const externalClickHandler = require('./externalClickHandler').default;
-  const handleExternalClick = externalClickHandler(store);
+      configureSentryScope((scope: Scope) => {
+        scope.setTag('context', 'content');
+      });
+    } catch (error) {
+      console.warn('Could not init Sentry in contentScript', error);
+    }
 
-  (window as CustomWindow).__LMEM__CONTENT_SCRIPT_INJECTED__ = true;
+    require('typeface-lato');
+    require('typeface-sedgwick-ave');
+    const store = require('./store').default;
+    const documentReady = require('../utils/documentReady').default;
+    const externalClickHandler = require('./externalClickHandler').default;
+    const handleExternalClick = externalClickHandler(store);
 
-  documentReady.then(() => {
-    document.addEventListener('click', handleExternalClick);
+    (window as CustomWindow).__LMEM__CONTENT_SCRIPT_INJECTED__ = true;
 
-    window.addEventListener('unload', () => {
-      document.removeEventListener('click', handleExternalClick);
+    documentReady.then(() => {
+      document.addEventListener('click', handleExternalClick);
+
+      window.addEventListener('unload', () => {
+        document.removeEventListener('click', handleExternalClick);
+      });
     });
-  });
+  } catch (e) {
+    captureException(e);
+  }
 }
