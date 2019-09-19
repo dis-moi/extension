a7ac4edf9e50c8d01b666da7c5627330af917fd7
lutangar <johan.dufour@gmail.com>
2019-09-19 16:48:28 +0200

    feat(update-message): prepare open onboarding button

--- src/app/content/App/Notice/Update/Update.tsx
+++ src/app/content/App/Notice/Update/Update.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { BackgroundButton } from 'components/atoms';
 
-export default () => (
+interface UpdateScreenProps {
+  openOnboarding: () => void;
+}
+
+export default ({ openOnboarding }: UpdateScreenProps) => (
   <div>
-    <h1>Le même en mieux</h1>
+    <h1>Le Même en Mieux</h1>
     <p>Bonjour, votre extension évolue.</p>
-    <p>Pour continuer à l&apos;utiliser, merci de mettre à jour.</p>
+    <p>Pour continuer à l&apos;utiliser, merci de la mettre à jour.</p>
+    <BackgroundButton onClick={openOnboarding}>Mettre à jour</BackgroundButton>
   </div>
 );
