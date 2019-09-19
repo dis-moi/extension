e984b251d83eedb5678163bfa31425108066e1f7
newick <erick@entre-quote.com>
2019-09-19 14:48:56 +0200

    fix(NotificationSubscriptionScreen): changed text

--- src/app/content/App/Subscriptions/Container.tsx
+++ src/app/content/App/Subscriptions/Container.tsx
@@ -1,13 +1,15 @@
 import styled from 'styled-components';
 
-export default styled.article`
+const Container = styled.article`
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding: 22px 37px 40px;
+  padding: 22px 20px 40px;
   text-align: center;
 
   & svg {
     margin-bottom: 35px;
   }
 `;
+
+export default Container;
--- src/app/content/App/Subscriptions/index.tsx
+++ src/app/content/App/Subscriptions/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
+import styled from 'styled-components';
 import BorderButton from 'components/atoms/Button/BorderButton/BorderButton';
 import withTitle from 'app/hocs/withTitle';
 import Illustration from './Illustration';
 import Container from './Container';
-import SubscriptionsData from './SubscriptionsData';
 import withConnect from './withConnect';
 import { compose } from 'redux';
 
@@ -11,18 +11,21 @@ interface SubscriptionsScreenProps {
   optionsRequested: () => void;
 }
 
+const SubscriptionInfo = styled.div`
+  margin-bottom: 35px;
+  font-size: 18px;
+`;
+
 export const Subscriptions = ({
   optionsRequested
 }: SubscriptionsScreenProps) => (
   <Container>
     <Illustration />
 
-    <SubscriptionsData>
-      <div>
-        <span>5</span>
-        <span>Abonnements</span>
-      </div>
-    </SubscriptionsData>
+    <SubscriptionInfo>
+      Votre extension est abonnée à <strong>8</strong> contributeurs sur{' '}
+      <strong>18</strong> possibles.
+    </SubscriptionInfo>
 
     <BorderButton onClick={optionsRequested}>Gérer</BorderButton>
   </Container>
