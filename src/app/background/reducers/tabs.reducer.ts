53260c6198a67750e6c1cb6beffdc5675db43d57
lutangar <johan.dufour@gmail.com>
2019-09-19 16:16:31 +0200

    feat(subscription): allow subscriptions from external sources and update options page accordingly

--- src/app/background/reducers/tabs.reducer.ts
+++ src/app/background/reducers/tabs.reducer.ts
@@ -6,6 +6,7 @@ import {
 } from 'app/constants/browser/tabs';
 import Tab from 'app/lmem/Tab';
 import { AppAction } from 'app/actions';
+import { isOptionsTabReadyAction } from '../sagas/sendContributorsToOptions.saga';
 
 export interface TabsState {
   [tabId: string]: Tab;
@@ -21,15 +22,27 @@ const removeTabFromList = (tab: Tab) => (state: TabsState): TabsState =>
 const markTabReady = (tab: Tab) => (state: TabsState): TabsState =>
   R.assocPath([tab.id.toString(), 'ready'], true, state);
 
+const markTabAsOptions = (tab: Tab) => (state: TabsState): TabsState =>
+  R.assocPath([tab.id.toString(), 'options'], true, state);
+
 export default function(state = initialState, action: AppAction) {
   switch (action.type) {
+    case 'OPTIONS_TAB_OPENED':
+      return markTabAsOptions(action.payload)(state);
     case TAB_CREATED:
     case TAB_UPDATED:
       return addTabToList(action.payload.tab)(state);
     case TAB_REMOVED:
       return removeTabFromList(action.payload.tab)(state);
     case 'LISTENING_ACTIONS_READY':
-      return action.meta.tab ? markTabReady(action.meta.tab)(state) : state;
+      return action.meta.tab
+        ? R.pipe(
+            markTabReady(action.meta.tab),
+            isOptionsTabReadyAction(action)
+              ? markTabAsOptions(action.meta.tab)
+              : R.identity
+          )(state)
+        : state;
 
     default:
       return state;
