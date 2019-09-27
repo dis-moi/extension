7a7ce6168d96e8581f36db281d1643036f7956ba
Jalil Arfaoui <jalil@arfaoui.net>
2019-09-27 16:56:32 +0200

    fix(account): fix broken links

--- src/app/content/App/Account/Menu/index.tsx
+++ src/app/content/App/Account/Menu/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link, RouteComponentProps } from 'react-router-dom';
-import { OpenButton } from 'components/atoms';
+import { ExternalLink, OpenButton } from 'components/atoms';
 import withTitle from 'app/hocs/withTitle';
 import Account from './Account';
 
@@ -8,14 +8,14 @@ type MenuProps = RouteComponentProps;
 
 export const Menu = ({ match }: MenuProps) => (
   <Account>
-    <Link to={`${match.url}/data`}>
+    <ExternalLink
+      target="_blank"
+      rel="noopener noreferrer"
+      href="http://www.bulles.fr/vie-privee"
+    >
       Mes données
       <OpenButton />
-    </Link>
-    <Link to={`${match.url}/contributions`}>
-      Mes contributions
-      <OpenButton />
-    </Link>
+    </ExternalLink>
     <Link to={`${match.url}/filters`}>
       Mes filtres
       <OpenButton />
