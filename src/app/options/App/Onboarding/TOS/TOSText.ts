e3a4630c423a94d327bac48ee090e7f2b43315a3
newick <erick@entre-quote.com>
2019-09-27 10:43:21 +0200

    fix(Onboarding text): changed text and links color

--- src/app/options/App/Onboarding/TOS/TOSText.ts
+++ src/app/options/App/Onboarding/TOS/TOSText.ts
@@ -2,6 +2,10 @@ import styled from 'styled-components';
 
 const TOSText = styled.p`
   font-size: 18px;
+
+  a {
+    color: ${props => props.theme.primaryColor};
+  }
 `;
 
 export default TOSText;
