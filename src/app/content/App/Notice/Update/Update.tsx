75d4f9baa79b99057d3544d3c83b382d409b0cbb
Erick Gardin <erick@entre-quote.com>
2019-09-27 16:59:11 +0200

    feat(bulles-update): service notice style (#391)
    * style(bulle of service): rendered as designed
    
    * fix(Bulle of service): @JalilArfaoui feedbacks, thanks

--- src/app/content/App/Notice/Update/Update.tsx
+++ src/app/content/App/Notice/Update/Update.tsx
@@ -1,15 +1,45 @@
 import React from 'react';
+import styled from 'styled-components';
 import { BackgroundButton } from 'components/atoms';
+import LmemLogo from 'assets/img/logo/logo-lmem-background.png';
+
+const Content = styled.section`
+  margin-top: 50px;
+  font-size: 14px;
+  color: ${props => props.theme.primaryColor};
+  text-align: center;
+`;
+
+const Title = styled.h1`
+  margin-top: 4px;
+  margin-bottom: 10px;
+  font-size: 22px;
+  font-weight: bold;
+`;
+
+const Text = styled.p`
+  margin: 0;
+
+  &:last-of-type {
+    margin-bottom: 34px;
+  }
+`;
+
+const Button = styled(BackgroundButton)`
+  margin-left: auto;
+  margin-right: auto;
+`;
 
 interface UpdateScreenProps {
   openOnboarding: () => void;
 }
 
 export default ({ openOnboarding }: UpdateScreenProps) => (
-  <div>
-    <h1>Le Même en Mieux</h1>
-    <p>Bonjour, votre extension évolue.</p>
-    <p>Pour continuer à l&apos;utiliser, merci de la mettre à jour.</p>
-    <BackgroundButton onClick={openOnboarding}>Mettre à jour</BackgroundButton>
-  </div>
+  <Content>
+    <img src={LmemLogo} alt="Le Même en Mieux" />
+    <Title>Le Même en Mieux</Title>
+    <Text>Bonjour, votre extension évolue.</Text>
+    <Text>Pour continuer à l&apos;utiliser, merci de la mettre à jour.</Text>
+    <Button onClick={openOnboarding}>Mettre à jour</Button>
+  </Content>
 );
