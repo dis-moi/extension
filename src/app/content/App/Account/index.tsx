9228996f3dce91415d769e1e577c84d694ece920
Jalil Arfaoui <jalil@arfaoui.net>
2019-09-27 22:56:07 +0200

    fix(account): fix broken links (#396)

--- src/app/content/App/Account/index.tsx
+++ src/app/content/App/Account/index.tsx
@@ -5,13 +5,11 @@ import Filters from './Filters/Filters';
 import Menu from './Menu';
 import About from './About';
 import Error from '../Error';
-import Loading from '../Loading';
 
 export default ({ match }: RouteComponentProps) => (
   <Switch>
     <Route path={match.url} exact component={Menu} />
     <Route path={`${match.url}/about`} component={About} />
-    <Route path={`${match.url}/privacy`} component={Loading} />
     <Route path={`${match.url}/filters`} component={Filters} />
     <Route component={Error} />
   </Switch>
