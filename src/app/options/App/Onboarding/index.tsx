47cbb6a1848d0a3b9540a531ad20907ebe1e5a3a
lutangar <johan.dufour@gmail.com>
2019-09-30 11:27:33 +0200

    feature(onboarding): handle scroll restoration between onboarding screens only

--- src/app/options/App/Onboarding/index.tsx
+++ src/app/options/App/Onboarding/index.tsx
@@ -4,11 +4,14 @@ import { RouteComponentProps } from 'react-router';
 import TOS from './TOS';
 import Subscribe from './Subscribe';
 import Examples from './Examples';
+import ScrollToTop from './ScrollToTop';
 
-export default ({ match }: RouteComponentProps) => (
-  <Switch>
-    <Route path={match.url} exact component={TOS} />
-    <Route path={`${match.url}/subscribe`} component={Subscribe} />
-    <Route path={`${match.url}/examples`} component={Examples} />
-  </Switch>
+export default ({ match, location }: RouteComponentProps) => (
+  <ScrollToTop location={location}>
+    <Switch>
+      <Route path={match.url} exact component={TOS} />
+      <Route path={`${match.url}/subscribe`} component={Subscribe} />
+      <Route path={`${match.url}/examples`} component={Examples} />
+    </Switch>
+  </ScrollToTop>
 );
