063e0802a179472270967751dcef3e92fa197ed2
lutangar <johan.dufour@gmail.com>
2019-10-01 11:57:52 +0200

    feat(options): use 'Lato' as default font face in options app

--- src/app/options/App/ScreenWrapper.ts
+++ src/app/options/App/ScreenWrapper.ts
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 export default styled.main`
   width: 1340px;
+  font-family: 'Lato', sans-serif;
   max-width: 100%;
   margin: 0 auto;
   background-color: #fff;
