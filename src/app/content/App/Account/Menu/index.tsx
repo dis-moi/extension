ad907b203effa1d303403c4c5a2a837cabff7f2c
newick <erick@entre-quote.com>
2019-10-03 14:28:29 +0200

    fix(extension menu): removed filters link

--- src/app/content/App/Account/Menu/index.tsx
+++ src/app/content/App/Account/Menu/index.tsx
@@ -16,10 +16,6 @@ export const Menu = ({ match }: MenuProps) => (
       Mes données
       <OpenButton />
     </ExternalLink>
-    <Link to={`${match.url}/filters`}>
-      Mes filtres
-      <OpenButton />
-    </Link>
     <Link to={`${match.url}/about`}>
       À propos
       <OpenButton />
