20c0ba404afd1aa9aac7c584b2a1b0efb37ef81c
Jalil Arfaoui <jalil@arfaoui.net>
2019-10-07 16:35:25 +0200

    fix(contribution):  still uses JotForm (#428)

--- src/app/content/App/Notice/List/NoNotice/index.tsx
+++ src/app/content/App/Notice/List/NoNotice/index.tsx
@@ -3,18 +3,14 @@ import Title from './Title';
 import ButtonContainer from './ButtonContainer';
 import Illustration from './Illustration';
 import BorderButton from 'components/atoms/Button/BorderButton';
+import { Link } from 'react-router-dom';
 
 export default () => (
   <>
     <Title>Pas de Bulle ici</Title>
     <ButtonContainer>
       <Illustration />
-      <BorderButton
-        as="a"
-        href="https://form.jotformeu.com/82702852284358"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
+      <BorderButton as={Link} to="/contribute">
         Créer une Bulle
       </BorderButton>
     </ButtonContainer>
