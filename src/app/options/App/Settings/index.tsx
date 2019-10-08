d4db05f76f5a50979b490dfc22b7bf844ae2bdb6
Jalil Arfaoui <jalil@arfaoui.net>
2019-10-08 12:11:52 +0200

    fix(options): browser icon bypasses TOS acceptation

--- src/app/options/App/Settings/index.tsx
+++ src/app/options/App/Settings/index.tsx
@@ -1,42 +1,50 @@
+import React from 'react';
 import { Redirect, Route, RouteComponentProps, Switch } from 'react-router';
+import ContributorNav from 'components/organisms/ContributorNav/ContributorNav';
+import { OptionsScreen } from '../../screens';
 import SubscriptionsScreen from './SubscriptionsScreen';
 import SuggestionsScreen from './SuggestionsScreen';
-import React from 'react';
 import Header from './Header';
-import ContributorNav from 'components/organisms/ContributorNav/ContributorNav';
 import withConnect from './withConnect';
-import { OptionsScreen } from '../../screens';
 
 interface Props extends RouteComponentProps {
   currentScreen: OptionsScreen | null;
   goToSubscriptions: () => void;
   goToSuggestions: () => void;
+  tosAccepted: boolean;
 }
 
 const UI = ({
   currentScreen,
   goToSuggestions,
   goToSubscriptions,
-  match
-}: Props) => (
-  <>
-    <Header />
+  match,
+  tosAccepted
+}: Props) =>
+  tosAccepted ? (
+    <>
+      <Header />
 
-    <ContributorNav
-      activeTab={currentScreen}
-      goToSubscriptions={goToSubscriptions}
-      goToSuggestions={goToSuggestions}
-    />
-
-    <Switch>
-      <Redirect exact path={match.url} to={`${match.url}/subscriptions`} />
-      <Route
-        path={`${match.url}/subscriptions`}
-        component={SubscriptionsScreen}
+      <ContributorNav
+        activeTab={currentScreen}
+        goToSubscriptions={goToSubscriptions}
+        goToSuggestions={goToSuggestions}
       />
-      <Route path={`${match.url}/suggestions`} component={SuggestionsScreen} />
-    </Switch>
-  </>
-);
+
+      <Switch>
+        <Redirect exact path={match.url} to={`${match.url}/subscriptions`} />
+        <Route
+          path={`${match.url}/subscriptions`}
+          component={SubscriptionsScreen}
+        />
+        <Route
+          path={`${match.url}/suggestions`}
+          component={SuggestionsScreen}
+        />
+      </Switch>
+    </>
+  ) : (
+    <Redirect to={'/onboarding'} />
+  );
 
 export default withConnect(UI);
