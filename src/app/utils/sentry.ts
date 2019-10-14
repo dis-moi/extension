4f7355ae91b44f52d4f8b074b30f19bc83420b0f
Jalil Arfaoui <jalil@arfaoui.net>
2019-10-14 22:06:31 +0200

    fix(sentry): disable sentry in content script on Firefox (#461)

--- src/app/utils/sentry.ts
+++ src/app/utils/sentry.ts
@@ -9,24 +9,31 @@ import {
 import { version } from '../../../package.json';
 import Logger from './Logger';
 
+let sentryInitialized = false;
+
 export const initSentry = () => {
   const blacklist = ['GlobalHandlers', 'ReportingObserver', 'CaptureConsole'];
   if (process.env.SENTRY_ENABLE) {
-    init({
-      dsn: process.env.SENTRY_DSN,
-      environment: process.env.NODE_ENV,
-      release: `${version}-${process.env.BUILD}`,
-      // defaultIntegrations: false,
-      integrations: integrations =>
-        integrations.filter(i => !blacklist.includes(i.name))
-    });
+    try {
+      init({
+        dsn: process.env.SENTRY_DSN,
+        environment: process.env.NODE_ENV,
+        release: `${version}-${process.env.BUILD}`,
+        integrations: integrations =>
+          integrations.filter(i => !blacklist.includes(i.name))
+      });
+      sentryInitialized = true;
+      Logger.info('Sentry initialized');
+    } catch (error) {
+      Logger.warn('Could not init Sentry in contentScript', error);
+    }
   }
 };
 
 export { Scope };
 type ScopeCallback = (scope: Scope) => void;
 export const configureSentryScope = (scopeCallback: ScopeCallback) => {
-  if (process.env.SENTRY_ENABLE) {
+  if (process.env.SENTRY_ENABLE && sentryInitialized) {
     configureScope(scopeCallback);
   }
 };
@@ -35,7 +42,7 @@ export const captureMessage = (
   message: string,
   level?: Severity
 ): string | undefined => {
-  if (process.env.SENTRY_ENABLE) {
+  if (process.env.SENTRY_ENABLE && sentryInitialized) {
     return sentryCaptureMessage(message, level);
   } else {
     switch (level) {
@@ -64,7 +71,7 @@ export const captureMessage = (
 };
 
 export const captureException = (exception: unknown): string | undefined => {
-  if (process.env.SENTRY_ENABLE) {
+  if (process.env.SENTRY_ENABLE && sentryInitialized) {
     return sentryCaptureException(exception);
   } else {
     Logger.error(exception);
