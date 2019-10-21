e0038c7e57463c494872da6fca714ffb236b6920
newick <erick@entre-quote.com>
2019-10-21 14:28:12 +0200

    feat(subscription): added case of no subscription feat(subscription): removed ul left padding

--- src/app/content/App/Subscriptions/Subscriptions.tsx
+++ src/app/content/App/Subscriptions/Subscriptions.tsx
@@ -3,6 +3,7 @@ import * as R from 'ramda';
 import styled from 'styled-components';
 import BackgroundButton from 'components/atoms/Button/BackgroundButton/BackgroundButton';
 
+import Illustration from './Illustration';
 import Container from './Container';
 import Avatar from '../../../../components/molecules/Avatar/Avatar';
 import { StatefulContributor } from '../../../lmem/contributor';
@@ -13,17 +14,34 @@ export interface SubscriptionsScreenProps {
   subscribedContributors: StatefulContributor[];
 }
 
-const SubscriptionList = styled.div`
+const Subscription = styled.div`
   margin-bottom: 20px;
+`;
 
-  ul {
-    display: flex;
-    list-style-type: none;
-  }
+const SubscriptionList = styled.ul`
+  display: flex;
+  padding-left: 0;
+  list-style-type: none;
+`;
 
-  li:not(:first-of-type) {
+const SubscriptionListItem = styled.li`
+  &:not(:first-of-type) {
     margin-left: 10px;
   }
+`;
+
+const SeeSubscriptions = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 40px;
+  height: 40px;
+  line-height: 1;
+  font-weight: bold;
+  background-color: ${props => props.theme.contributorGrey};
+  border: none;
+  border-radius: 50%;
+  cursor: pointer;
 `;
 
 const SubscriptionInfo = styled.div`
@@ -41,42 +59,56 @@ const Subscriptions = ({
   subscribedContributors
 }: SubscriptionsScreenProps) => (
   <Container>
-    <SubscriptionList>
+    <Subscription>
+      {subscribedContributors.length === 0 && <Illustration />}
       {R.splitEvery(
-          nbContributorsPerRow,
-          subscribedContributors
-            .slice(0, nbContributorsPerRow * maxNbRows)
-            .filter(
-              (contributor, i, slicedSubscribedContributors) =>
-                !(
-                  slicedSubscribedContributors.length % nbContributorsPerRow ===
-                    0 && i === slicedSubscribedContributors.length - 1
-                )
-            )
-        ).map((contributorsChunk, chunkIndex, slicedSubscribedContributors) => (
-            <ul key={`chunk${chunkIndex}`}>
-            {contributorsChunk.map(contributor => (
-                <li key={`contributor${contributor.id}`}>
-                    <Avatar contributor={contributor} size="small" />
-                </li>
-            ))}
-            {chunkIndex === slicedSubscribedContributors.length - 1 && (
-              <li>
-                <button onClick={openSubscriptions}>...</button>
-              </li>
-            )}
-          </ul>
-        ))}
-      </SubscriptionList>
+        nbContributorsPerRow,
+        subscribedContributors
+          .slice(0, nbContributorsPerRow * maxNbRows)
+          .filter(
+            (contributor, i, slicedSubscribedContributors) =>
+              !(
+                slicedSubscribedContributors.length % nbContributorsPerRow ===
+                  0 && i === slicedSubscribedContributors.length - 1
+              )
+          )
+      ).map((contributorsChunk, chunkIndex, slicedSubscribedContributors) => (
+        <SubscriptionList key={`chunk${chunkIndex}`}>
+          {contributorsChunk.map(contributor => (
+            <SubscriptionListItem key={`contributor${contributor.id}`}>
+              <Avatar contributor={contributor} size="small" />
+            </SubscriptionListItem>
+          ))}
+          {chunkIndex === slicedSubscribedContributors.length - 1 && (
+            <SubscriptionListItem>
+              <SeeSubscriptions onClick={openSubscriptions} title="Voir tout">
+                ...
+              </SeeSubscriptions>
+            </SubscriptionListItem>
+          )}
+        </SubscriptionList>
+      ))}
+    </Subscription>
 
-      <SubscriptionInfo>
-          Vous êtes abonné(e)s à <strong>{subscribedContributors.length}</strong>{' '}
-          contributeur(ice){pluralize(subscribedContributors.length)}.
-      </SubscriptionInfo>
+    <SubscriptionInfo>
+      {subscribedContributors.length === 0 && (
+        <>
+          Vous n&apos;êtes abonné·e·s à aucun·e contributeur·ice, c&apos;est
+          nécessaire au bon fonctionnement de l&apos;extension.
+        </>
+      )}
+      {subscribedContributors.length > 0 && (
+        <>
+          Vous êtes abonné(e)s à{' '}
+          <strong>{subscribedContributors.length}</strong> contributeur(ice)
+          {pluralize(subscribedContributors.length)}.
+        </>
+      )}
+    </SubscriptionInfo>
 
-      <BackgroundButton onClick={openSubscriptions}>
-          Gérer mes abonnements
-      </BackgroundButton>
+    <BackgroundButton onClick={openSubscriptions}>
+      Gérer mes abonnements
+    </BackgroundButton>
   </Container>
 );
 
