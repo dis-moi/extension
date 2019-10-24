2bad6e5751ea882b3f883f150c54b9329fe3c5e4
Jalil Arfaoui <jalil@arfaoui.net>
2019-10-24 14:08:21 +0200

    refactor(analytics): refactor contributors ratings

--- src/app/actions/subscription.ts
+++ src/app/actions/subscription.ts
@@ -1,20 +1,13 @@
 import { Contributor, ContributorId } from 'app/lmem/contributor';
 import { SUBSCRIBE, UNSUBSCRIBE } from 'app/constants/ActionTypes';
-import ContributorRatingType from 'app/lmem/ContributorRatingType';
 import { ActionMeta, BaseAction } from '.';
 
 export interface ContributorAction extends BaseAction {
   payload: { contributor: Contributor | ContributorId };
-  meta: ActionMeta & {
-    ratingType?: ContributorRatingType;
-  };
 }
 
 export interface SubscribeAction extends ContributorAction {
   type: typeof SUBSCRIBE;
-  meta: ActionMeta & {
-    ratingType: ContributorRatingType.SUBSCRIBE;
-  };
 }
 export const subscribe = (
   contributor: Contributor | ContributorId,
@@ -22,17 +15,11 @@ export const subscribe = (
 ): SubscribeAction => ({
   type: SUBSCRIBE,
   payload: { contributor },
-  meta: {
-    ...meta,
-    ratingType: ContributorRatingType.SUBSCRIBE
-  }
+  meta
 });
 
 export interface UnsubscribeAction extends ContributorAction {
   type: typeof UNSUBSCRIBE;
-  meta: ActionMeta & {
-    ratingType: ContributorRatingType.UNSUBSCRIBE;
-  };
 }
 export const unsubscribe = (
   contributor: Contributor,
@@ -40,8 +27,5 @@ export const unsubscribe = (
 ): UnsubscribeAction => ({
   type: UNSUBSCRIBE,
   payload: { contributor },
-  meta: {
-    ...meta,
-    ratingType: ContributorRatingType.UNSUBSCRIBE
-  }
+  meta
 });
