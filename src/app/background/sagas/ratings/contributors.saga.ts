9a710abd2fade28d2638d80a157396c00af650e4
Jalil Arfaoui <jalil@arfaoui.net>
2019-10-24 14:08:21 +0200

    refactor(analytics): refactor contributors ratings

--- src/app/background/sagas/ratings/contributors.saga.ts
+++ src/app/background/sagas/ratings/contributors.saga.ts
@@ -1,42 +1,37 @@
 import { SagaIterator } from '@redux-saga/types';
-import { takeEvery, call } from '@redux-saga/core/effects';
-import * as R from 'ramda';
-
-import { AppAction, ContributorAction } from 'app/actions';
+import { call, takeEvery } from '@redux-saga/core/effects';
+import { ContributorAction } from 'app/actions';
 import { captureException } from 'app/utils/sentry';
 import ContributorRatingType from 'app/lmem/ContributorRatingType';
 import postContributorRating from 'api/postContributorRating';
 import { getContributorId } from 'app/background/reducers/subscriptions.reducer';
+import { SUBSCRIBE, UNSUBSCRIBE } from 'app/constants/ActionTypes';
 
-export const isContributorRatingTypeAction = (action: AppAction) =>
-  R.hasPath(['payload', 'contributor'], action) &&
-  R.hasPath(['meta', 'ratingType'], action) &&
-  Object.values(ContributorRatingType).includes(
-    R.path(['meta', 'ratingType'], action)
-  );
-
-export function* contributorRatingSaga(
-  action: ContributorAction
-): SagaIterator {
-  try {
-    const {
-      meta: { ratingType }
-    } = action;
-    if (ratingType) {
-      yield call(postContributorRating, {
-        contributorId: getContributorId(action),
-        ratingType
-      });
-    } else {
-      throw new Error(
-        `Contributor rating saga caught an action (${action.type}) with no ratings, this is not supposed to happen.`
-      );
+const postContributorFeedback = (ratingType: ContributorRatingType) =>
+  function* contributorRatingSaga(action: ContributorAction): SagaIterator {
+    try {
+      if (ratingType) {
+        yield call(postContributorRating, {
+          contributorId: getContributorId(action),
+          ratingType
+        });
+      } else {
+        throw new Error(
+          `Contributor rating saga caught an action (${action.type}) with no ratings, this is not supposed to happen.`
+        );
+      }
+    } catch (e) {
+      captureException(e);
     }
-  } catch (e) {
-    captureException(e);
-  }
-}
+  };
 
 export default function* contributorRatingsRootSaga() {
-  yield takeEvery(isContributorRatingTypeAction, contributorRatingSaga);
+  yield takeEvery(
+    SUBSCRIBE,
+    postContributorFeedback(ContributorRatingType.SUBSCRIBE)
+  );
+  yield takeEvery(
+    UNSUBSCRIBE,
+    postContributorFeedback(ContributorRatingType.UNSUBSCRIBE)
+  );
 }
