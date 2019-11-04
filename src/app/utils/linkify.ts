44f9fdabf625bc69ebfca0b1d27e16050ca09008
lutangar <johan.dufour@gmail.com>
2019-11-04 12:08:07 +0100

    feat(preview): add html clickable link in preview (cheap version)

---
+++ src/app/utils/linkify.ts
@@ -0,0 +1,5 @@
+// eslint-disable-next-line no-useless-escape
+const LINK_DETECTION_REGEX = /(([a-z]+:\/\/)?(([a-z0-9\-]+\.)+([a-z]{2}|aero|arpa|biz|com|coop|edu|gov|info|int|jobs|mil|museum|name|nato|net|org|pro|travel|local|internal))(:[0-9]{1,5})?(\/[a-z0-9_\-\.~]+)*(\/([a-z0-9_\-\.]*)(\?[a-z0-9+_\-\.%=&amp;]*)?)?(#[a-zA-Z0-9!$&'()*+.=-_~:@/?]*)?)(\s+|$)/gi;
+
+export default (text: string) =>
+  text.replace(LINK_DETECTION_REGEX, url => `<a href="${url}">${url}</a>`);
