a7216cdc045f29cad2f1d593ee725fd6746cda63
newick <erick@entre-quote.com>
2019-11-04 14:36:45 +0100

    fix(Subscrition screen): removed useless SVG margin

--- src/app/content/App/Subscriptions/Container.ts
+++ src/app/content/App/Subscriptions/Container.ts
@@ -6,10 +6,6 @@ const Container = styled.article`
   align-items: center;
   padding: 22px 20px 40px;
   text-align: center;
-
-  & svg {
-    margin-bottom: 35px;
-  }
 `;
 
 export default Container;
