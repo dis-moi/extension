7cde9f421ccf651dad1b977d7093076656ee0142
newick <erick@entre-quote.com>
2019-11-05 11:33:46 +0100

    feat(header links): added and designed links

--- src/app/options/App/Settings/Header/Header.tsx
+++ src/app/options/App/Settings/Header/Header.tsx
@@ -1,10 +1,28 @@
 import React from 'react';
+import styled from 'styled-components';
 import Logo from 'components/atoms/LogoBeta';
 import LogoWrapper from './LogoWrapper';
+import { ExternalLink } from 'components/atoms';
+
+const HeaderNav = styled.nav``;
+
+const HeaderNavItem = styled(ExternalLink)`
+  font-size: 16px;
+  color: ${props => props.theme.primaryColor} & + & {
+    margin-left: 16px;
+  }
+`;
 
 const Header = () => (
   <LogoWrapper>
     <Logo />
+
+    <HeaderNav>
+      <HeaderNavItem href="https://www.bulles.fr/devenir-contributeur/">
+        Devenir contributeur
+      </HeaderNavItem>
+      <HeaderNavItem href="https://www.bulles.fr/aide/">Aide</HeaderNavItem>
+    </HeaderNav>
   </LogoWrapper>
 );
 
