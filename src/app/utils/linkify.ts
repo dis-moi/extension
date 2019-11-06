2d9a4c7dba327502d51d49d3ec1ac7de85ecaeaf
lutangar <johan.dufour@gmail.com>
2019-11-06 12:20:52 +0100

    fix(link-in-preview): add target blank and same rel directives as external links

--- src/app/utils/linkify.ts
+++ src/app/utils/linkify.ts
@@ -2,4 +2,8 @@
 const LINK_DETECTION_REGEX = /(([a-z]+:\/\/)?(([a-z0-9\-]+\.)+([a-z]{2}|aero|arpa|biz|com|coop|edu|gov|info|int|jobs|mil|museum|name|nato|net|org|pro|travel|local|internal))(:[0-9]{1,5})?(\/[a-z0-9_\-\.~]+)*(\/([a-z0-9_\-\.]*)(\?[a-z0-9+_\-\.%=&amp;]*)?)?(#[a-zA-Z0-9!$&'()*+.=-_~:@/?]*)?)(\s+|$)/gi;
 
 export default (text: string) =>
-  text.replace(LINK_DETECTION_REGEX, url => `<a href="${url}">${url}</a>`);
+  text.replace(
+    LINK_DETECTION_REGEX,
+    url =>
+      `<a target="_blank" rel="noopener noreferrer" href="${url}">${url}</a>`
+  );
