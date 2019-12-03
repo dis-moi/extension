8cc2b9bdeef2e761ae3b607fe5b58d7f14d4db06
lutangar <johan.dufour@gmail.com>
2019-12-03 11:06:57 +0100

    feat(storage): switch from "local storage" to "browser sync storage"

--- src/app/background/store/migrations/index.ts
+++ src/app/background/store/migrations/index.ts
@@ -1,10 +1,18 @@
-import { createMigrate } from 'redux-persist';
-import { MigrationManifest } from 'redux-persist/es/types';
+import {
+  createMigrate as reduxPersistCreateMigrate,
+  MigrationManifest,
+  MigrationConfig,
+  PersistedState,
+  getStoredState,
+  purgeStoredState
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage
 import { migration1 } from './1.2017-01-26';
 import { migration2 } from './2.2019-02-11';
 import { migration3 } from './3.2019-03';
 import { migration4 } from './4.2019-08-28';
 import { migration5 } from './5.2019-09-12';
+import { persistConfig } from '../';
 
 const migrations: MigrationManifest = {
   1: migration1, // 26-01-2017
@@ -12,6 +20,30 @@ const migrations: MigrationManifest = {
   3: migration3, // March 2019 - Typescript migration
   4: migration4, // August 2019 - rename readNotices to markedReadNotices
   5: migration5 // Septembre 2019 - rename markedReadNotices back to readNotices
+};
+
+export const isStateEmpty = (state: unknown) => {
+  return !state;
+};
+
+export const createMigrate = (
+  migrations: MigrationManifest,
+  config: MigrationConfig
+) => {
+  return async (state: PersistedState, currentVersion: number) => {
+    // if the sync storage is empty, let's try to restore a previous state from the local storage
+    if (isStateEmpty(state)) {
+      const localStoragePersistConfig = {
+        ...persistConfig,
+        storage
+      };
+
+      state = await getStoredState(localStoragePersistConfig);
+      purgeStoredState(localStoragePersistConfig);
+    }
+
+    return reduxPersistCreateMigrate(migrations, config)(state, currentVersion);
+  };
 };
 
 export const migrate = createMigrate(migrations, {
