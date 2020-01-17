80aed5a246fedb9ec355ee3d5bfc05a3cfb1674d
newick <erick@entre-quote.com>
2020-01-17 17:13:27 +0100

    feat(question): styled as designed

--- src/app/content/App/Question/ConfirmationScreen/Content.ts
+++ src/app/content/App/Question/ConfirmationScreen/Content.ts
@@ -2,5 +2,5 @@ import styled from 'styled-components';
 
 export default styled.p`
   font-size: 14px;
-  margin: 5px 0 20px;
+  margin: 5px 0 40px;
 `;
--- src/components/atoms/Forms/Error/Error.ts
+++ src/components/atoms/Forms/Error/Error.ts
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 export default styled.p`
+  margin-top: 0;
   font-size: 13px;
   line-height: 1;
   color: ${props => props.theme.formError};
