7a93fc5a63122538466df64d798528df46e19bc1
lutangar <johan.dufour@gmail.com>
2020-02-03 11:20:26 +0100

    chore(subscriptions): remove contributors ratings

--- src/app/background/sagas/ratings/index.ts
+++ src/app/background/sagas/ratings/index.ts
@@ -1,7 +1,6 @@
 import { fork, all } from 'redux-saga/effects';
-import contributors from './contributors.saga';
 import notices from './notices.saga';
 
 export default function* rootSaga() {
-  yield all([fork(contributors), fork(notices)]);
+  yield all([fork(notices)]);
 }
