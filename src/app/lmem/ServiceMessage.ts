15301070677f481c311ede77624e2c04c1e3698f
lutangar <johan.dufour@gmail.com>
2020-02-26 11:08:48 +0100

    refactor(serviceMessage): introduce `LinkType` for a service message

--- src/app/lmem/ServiceMessage.ts
+++ src/app/lmem/ServiceMessage.ts
@@ -1,6 +1,13 @@
+export enum LinkType {
+  Options = 'options',
+  Internal = 'internal',
+  External = 'external'
+}
+
 export interface ServiceMessageAction {
   label: string;
   url: string;
+  type: LinkType;
 }
 
 export default interface ServiceMessageState {
