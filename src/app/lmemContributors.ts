6c32ab601dabc74f1220521a9ed308380145c609
lutangar <johan.dufour@gmail.com>
2020-03-02 10:38:20 +0100

    feat(subscriptions): auto-subscribe to a selection of contributors on installation (instead of auto-subscribe to all contributors)
    feat(subscriptions): auto-subscribe to a selection of contributors on installation
    (instead of auto-subscribe to all contributors)

--- src/app/lmemContributors.ts
+++ src/app/lmemContributors.ts
@@ -1,2 +1,26 @@
 // @todo plz remove me after the beta
 export const lmemContributorIds = [24, 25, 16];
+
+// @todo remove this one shot feature
+export const preselectedContributorIds = [
+  13,
+  14,
+  16,
+  21,
+  24,
+  25,
+  27,
+  29,
+  30,
+  31,
+  32,
+  33,
+  36,
+  42,
+  43,
+  44,
+  46,
+  49,
+  50,
+  51
+];
