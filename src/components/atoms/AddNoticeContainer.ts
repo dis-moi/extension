e963ce2763e87a2570e17e576f568d9b46c73400
newick <erick@entre-quote.com>
2020-03-03 14:56:19 +0100

    feature(ServiceMessageLine): created and positionned ServiceMessageLine

--- src/components/atoms/AddNoticeContainer.ts
+++ src/components/atoms/AddNoticeContainer.ts
@@ -4,6 +4,5 @@ import CenterContainer from './CenterContainer';
 export default styled(CenterContainer)`
   width: 100%;
   position: absolute;
-  bottom: 0;
-  margin: 25px 0;
+  bottom: 60px;
 `;
---
+++ src/components/molecules/ServiceMessageLine/ServiceMessageLine.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import styled from 'styled-components';
+import { ExternalLink } from '../../atoms';
+
+const ServiceMessageLineMessage = styled.div`
+  position: absolute;
+  bottom: 8px;
+  width: 100%;
+  padding-top: 8px;
+  font-size: 12px;
+  font-weight: bold;
+  text-align: center;
+  border-top: 1px solid ${props => props.theme.navInactive};
+`;
+
+const ServiceMessageLine = () => {
+  return (
+    <ServiceMessageLineMessage>
+      Bulles devient Dismoi -{' '}
+      <ExternalLink href="dzadazd">en savoir plus</ExternalLink>
+    </ServiceMessageLineMessage>
+  );
+};
+
+export default ServiceMessageLine;
