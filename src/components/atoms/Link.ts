8585be93bc0bf79b9531f6c42992fbddf98bd85f
lutangar <johan.dufour@gmail.com>
2020-03-03 15:22:01 +0100

    fix(links): fix link style not being applied

--- src/components/atoms/Link.ts
+++ src/components/atoms/Link.ts
@@ -1,7 +1,8 @@
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import { style } from './Anchor';
+import { Theme } from 'app/theme';
 
 export default styled(Link)`
-  ${style}
+  ${({ theme }: { theme: Theme }) => style({ theme })}
 `;
