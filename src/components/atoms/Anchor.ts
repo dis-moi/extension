09bec04bc8ab2cb28df7f1bca526b2f0e2ccfe75
lutangar <johan.dufour@gmail.com>
2020-03-03 15:22:01 +0100

    fix(links): fix link style not being applied

--- src/components/atoms/Anchor.ts
+++ src/components/atoms/Anchor.ts
@@ -1,17 +1,17 @@
 import styled from 'styled-components';
-import { Theme } from '../../app/theme';
+import { Theme } from 'app/theme';
 
-export const style = `
-  color: ${({ theme }: { theme: Theme }) => theme.activeColor};
+export const style = ({ theme }: { theme: Theme }) => `
+  color: ${theme.activeColor};
   text-decoration: underline;
   cursor: pointer;
   transition: all 0.2s ease-in-out;
 
   &:hover {
-    color: ${({ theme }: { theme: Theme }) => theme.secondaryColor};
+    color: ${theme.secondaryColor};
   }
 `;
 
 export default styled.a`
-  ${style}
+  ${({ theme }: { theme: Theme }) => style({ theme })}
 `;
