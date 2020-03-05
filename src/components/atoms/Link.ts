68b8acf57ad95c5e462da46602e7d7c7282f7955
lutangar <johan.dufour@gmail.com>
2020-03-05 14:25:27 +0100

    chore(link): simplify link style

--- src/components/atoms/Link.ts
+++ src/components/atoms/Link.ts
@@ -1,8 +1,7 @@
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import { style } from './Anchor';
-import { Theme } from 'app/theme';
 
 export default styled(Link)`
-  ${({ theme }: { theme: Theme }) => style({ theme })}
+  ${style}
 `;
