44c7826cc856e47398bad7c131a72bc9f870643e
lutangar <johan.dufour@gmail.com>
2020-05-08 16:45:22 +0200

    chore: Take extension id from env.
    Might require more thorough testing...

--- src/app/profiles/extensionId.ts
+++ src/app/profiles/extensionId.ts
@@ -1,3 +1,5 @@
-const extensionId = 'ppimadjcdfncbgjffmdignpfnmfkakdl'; // @todo
+const extensionId = chrome
+  ? process.env.CHROME_EXTENSION_ID
+  : process.env.FIREFOX_EXTENSION_ID;
 
 export default extensionId;
