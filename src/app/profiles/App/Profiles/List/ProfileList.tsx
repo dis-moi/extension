7dc06f05c35526df7523c077dd5664fd953199ee
newick <erick@entre-quote.com>
2020-05-13 16:18:16 +0200

    feat(ProfileList): changes Link atom link

--- src/app/profiles/App/Profiles/List/ProfileList.tsx
+++ src/app/profiles/App/Profiles/List/ProfileList.tsx
@@ -4,7 +4,7 @@ import { Arrow, Loading } from 'components/atoms/icons';
 import { trilean } from 'types';
 import ContributorLarge from 'components/organisms/Contributor/ContributorLarge';
 import ContributorsList from 'components/organisms/Contributor/ContributorsList';
-import Link from 'components/atoms/Link';
+import Link from 'components/atoms/Link/Link';
 
 export interface ProfileListProps {
   loading: trilean;
