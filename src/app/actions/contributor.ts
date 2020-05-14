e6546efba574405ac1a9cce06d0b40fb93ceda2b
lutangar <johan.dufour@gmail.com>
2020-05-14 19:27:04 +0200

    feat(profiles): Add fetch contributor saga

---
+++ src/app/actions/contributor.ts
@@ -0,0 +1,48 @@
+import { ActionMeta, BaseAction, ErrorAction } from 'app/actions';
+import { ContributorId } from 'app/lmem/contributor';
+import { Level } from 'app/utils/Logger';
+
+export const FETCH_CONTRIBUTOR_REQUEST = 'FETCH_CONTRIBUTOR_REQUEST';
+export interface FetchContributorRequestAction extends BaseAction {
+  type: typeof FETCH_CONTRIBUTOR_REQUEST;
+  payload: ContributorId;
+}
+export const fetchContributorRequest = (
+  contributorId: ContributorId,
+  meta?: ActionMeta
+): FetchContributorRequestAction => ({
+  type: FETCH_CONTRIBUTOR_REQUEST,
+  payload: contributorId,
+  meta
+});
+
+export const FETCH_CONTRIBUTOR_SUCCESS = 'FETCH_CONTRIBUTOR_SUCCESS';
+export interface FetchContributorSuccessAction extends BaseAction {
+  type: typeof FETCH_CONTRIBUTOR_SUCCESS;
+  payload: ContributorId;
+}
+export const fetchContributorSuccess = (
+  contributorId: ContributorId,
+  meta?: ActionMeta
+): FetchContributorSuccessAction => ({
+  type: FETCH_CONTRIBUTOR_SUCCESS,
+  payload: contributorId,
+  meta
+});
+
+export const FETCH_CONTRIBUTOR_FAILURE = 'FETCH_CONTRIBUTOR_FAILURE';
+export interface FetchContributorFailureAction extends ErrorAction {
+  type: typeof FETCH_CONTRIBUTOR_FAILURE;
+}
+export const fetchContributorFailure = (
+  e: Error,
+  meta?: ActionMeta
+): FetchContributorFailureAction => ({
+  type: FETCH_CONTRIBUTOR_FAILURE,
+  payload: e,
+  error: true,
+  meta: {
+    ...meta,
+    severity: Level.ERROR
+  }
+});
--- src/app/profiles/store/sagas/notices.saga.ts
+++ src/app/profiles/store/sagas/notices.saga.ts
@@ -1,4 +1,5 @@
 import { call, take, select, put, takeLatest } from 'redux-saga/effects';
+import { createMatchSelector, LOCATION_CHANGE } from 'connected-react-router';
 import {
   FETCH_NOTICES_REQUEST,
   fetchNoticesRequest,
@@ -6,8 +7,10 @@ import {
 } from 'app/actions';
 import {
   FETCH_CONTRIBUTOR_NOTICES,
+  fetchContributorNotices,
   FetchContributorNoticesAction
 } from '../actions';
+import { fetchContributorRequest } from 'app/actions/contributor';
 import { getContributorById } from 'app/profiles/store/selectors';
 import { ContributorId } from 'app/lmem/contributor';
 import fetchNoticesSaga from 'app/store/sagas/fetchNotices.saga';
@@ -28,7 +31,17 @@ function* fetchContributorNoticesSaga({
   yield put(fetchNoticesRequest(Object.values(contributor.noticesUrls)));
 }
 
+function* locationChangeSaga() {
+  const matchSelector = createMatchSelector('/les-contributeurs/:id');
+  const match = yield select(matchSelector);
+  if (match) {
+    yield put(fetchContributorRequest(Number(match.params.id)));
+    yield put(fetchContributorNotices(Number(match.params.id)));
+  }
+}
+
 export default function* noticesRootSaga() {
   yield takeLatest(FETCH_CONTRIBUTOR_NOTICES, fetchContributorNoticesSaga);
   yield takeLatest(FETCH_NOTICES_REQUEST, fetchNoticesSaga);
+  yield takeLatest(LOCATION_CHANGE, locationChangeSaga);
 }
