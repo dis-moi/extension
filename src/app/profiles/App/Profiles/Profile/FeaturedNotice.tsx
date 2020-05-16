cf0f4bd2acc54f722fe0ed70bb3338ab4a77a34c
newick <erick@entre-quote.com>
2020-05-16 15:33:15 +0200

    fix(FeatureNotice): removed wonderful red background

--- src/app/profiles/App/Profiles/Profile/FeaturedNotice.tsx
+++ src/app/profiles/App/Profiles/Profile/FeaturedNotice.tsx
@@ -1,8 +1,6 @@
 import styled from 'styled-components';
 import { ProfileNoticeListItem } from './ProfileNoticeListItem';
 
-const FeaturedNotice = styled(ProfileNoticeListItem)`
-  background-color: red;
-`;
+const FeaturedNotice = styled(ProfileNoticeListItem)``;
 
 export default FeaturedNotice;
