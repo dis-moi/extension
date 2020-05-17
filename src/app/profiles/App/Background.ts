0392225f9084812121af86d852cc3180659ce5ae
newick <erick@entre-quote.com>
2020-05-17 16:26:28 +0200

    feat(Profile): styles default background and container

--- src/app/profiles/App/Background.ts
+++ src/app/profiles/App/Background.ts
@@ -2,6 +2,10 @@ import styled from 'styled-components';
 
 const Background = styled.div`
   background-color: ${props => props.theme.noticeBg};
+
+  ////////////////////////////////////////////
+  margin: -8px; // TO DELETE when on WordPress
+  ////////////////////////////////////////////
 `;
 
 export default Background;
--- src/app/profiles/App/PageContainer.tsx
+++ src/app/profiles/App/PageContainer.tsx
@@ -3,15 +3,16 @@ import styled from 'styled-components';
 const PageContainer = styled.div`
   box-sizing: border-box;
   max-width: 100%;
-  width: 992px;
+  width: 1032px;
+  min-height: 100vh;
   margin: 0 auto;
+  padding-right: 20px;
+  padding-left: 20px;
   padding-bottom: 70px;
   font-family: 'Lato', sans-serif;
 
-  @media (max-width: 1052px) {
-    padding-right: ${props => props.theme.marginS};
+  @media (max-width: ${props => props.theme.desktopWidth}) {
     padding-bottom: 50px;
-    padding-left: ${props => props.theme.marginS};
   }
 `;
 
