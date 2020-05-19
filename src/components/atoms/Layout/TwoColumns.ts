29b61871eff656cf73c765b4586e0106749e468b
newick <erick@entre-quote.com>
2020-05-19 15:30:29 +0200

    fix(TwoColumns): changes mainColumn width to prevent issues with white-space

--- src/components/atoms/Layout/TwoColumns.ts
+++ src/components/atoms/Layout/TwoColumns.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 const TwoColumns = styled.div`
   display: grid;
-  grid-template-columns: auto 310px;
+  grid-template-columns: minmax(0, 1fr) 310px;
   grid-column-gap: ${props => props.theme.marginM};
 
   @media (max-width: ${props => props.theme.desktopWidth}) {
