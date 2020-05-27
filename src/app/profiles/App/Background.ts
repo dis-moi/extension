2783d647af8783e546fd50e801dd7e5a2baf371c
newick <erick@entre-quote.com>
2020-05-27 15:12:24 +0200

    fix(Profiles): removed Background margin

--- src/app/profiles/App/Background.ts
+++ src/app/profiles/App/Background.ts
@@ -2,10 +2,6 @@ import styled from 'styled-components';
 
 const Background = styled.div`
   background-color: ${props => props.theme.noticeBg};
-
-  ////////////////////////////////////////////
-  margin: -8px; // TO DELETE when on WordPress
-  ////////////////////////////////////////////
 `;
 
 export default Background;
