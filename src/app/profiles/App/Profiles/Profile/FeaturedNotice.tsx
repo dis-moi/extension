fc2ac965baecbabc6129f566caac678f93c1a504
newick <erick@entre-quote.com>
2020-08-24 10:57:55 +0200

    fix(FeaturedNotice): aligns image on the left for mobiles

--- src/app/profiles/App/Profiles/Profile/FeaturedNotice.tsx
+++ src/app/profiles/App/Profiles/Profile/FeaturedNotice.tsx
@@ -3,6 +3,21 @@ import { ProfileNoticeListItem } from './ProfileNoticeListItem';
 
 const FeaturedNotice = styled(ProfileNoticeListItem).attrs({ featured: true })`
   margin-bottom: 40px;
+
+  img {
+    margin-bottom: 10px;
+  }
+
+  @media (max-width: ${props => props.theme.tabletWidth}) {
+    display: flex;
+    flex-direction: column;
+
+    img {
+      align-self: flex-end;
+      width: auto !important;
+      max-height: 105vw;
+    }
+  }
 `;
 
 export default FeaturedNotice;
