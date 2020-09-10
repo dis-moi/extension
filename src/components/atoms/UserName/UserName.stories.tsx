8a72ca52e320a356b615143e7d1aa19537499332
newick <erick@entre-quote.com>
2020-09-10 16:30:45 +0200

    feat(SEO): duplicates UserName code for contributor compact

--- src/components/atoms/UserName/UserName.stories.tsx
+++ src/components/atoms/UserName/UserName.stories.tsx
@@ -2,10 +2,14 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withKnobs, text } from '@storybook/addon-knobs';
 import UserName from './UserName';
+import UserNameCompact from './UserNameCompact';
 import Faker from 'faker';
 
 const defaultUserName = Faker.name.findName();
 
 storiesOf('Components/Atoms/UserName', module)
   .addDecorator(withKnobs)
-  .add('normal', () => <UserName>{text('content', defaultUserName)}</UserName>);
+  .add('normal', () => <UserName>{text('content', defaultUserName)}</UserName>)
+  .add('compact', () => (
+    <UserNameCompact>{text('content', defaultUserName)}</UserNameCompact>
+  ));
