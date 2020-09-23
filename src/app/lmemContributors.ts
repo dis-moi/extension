9833bb4195fd04248d23866ebbf1f319b1c3f33d
Maarten <maarten@lmem.net>
2020-09-23 14:44:57 +0200

    Update lmemContributors.ts
    - delete individuals
    - add new contributors created last month

--- src/app/lmemContributors.ts
+++ src/app/lmemContributors.ts
@@ -1,27 +1,23 @@
 // @todo remove this one shot feature
 export const preselectedContributorIds = [
-  13,
-  14,
-  16,
-  21,
-  24,
-  25,
-  27,
-  29,
-  30,
-  31,
-  32,
-  33,
-  36,
-  42,
-  43,
-  44,
-  46,
-  49,
-  50,
-  53,
-  54,
+  16, // Le Même en Mieux
+  24, // Alertoo
+  25, // Le Même en Local
+  27, // Info ou Intox ?
+  29, // Equipe Dismoi
+  31, // Alternatives gratuites
+  32, // Colibri Ecolo
+  42, // On en parle
+  43, // D'occasion ?
+  44, // Droit de réponse
+  46, // Selon Les Inroks
+  50, // Des casseroles
+  53, // Selon Que Choisir
+  54, // 60 Millions de Consommateurs
   55, // CaptainFact.io
-  59, // LeMonde avec Wirecutter
-  60 // Amazon antidote
+  59, // Selon Le Monde
+  60, // Amazon antidote
+  65, // Selon Micode
+  66, // Le Même en Open Source
+  67 // Le Même en  Moins Cher
 ];
