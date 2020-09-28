95b846cb6a945a9a6ac18b3a62c14b4a570f4f74
lutangar <johan.dufour@gmail.com>
2020-09-28 12:08:36 +0200

    chore(username): Refactor to avoid duplication

--- src/components/atoms/UserName/UserNameCompact.ts
+++ src/components/atoms/UserName/UserNameCompact.ts
@@ -1,25 +1,15 @@
 import styled from 'styled-components';
-import { Link } from '../Link';
+import UserName from './UserName';
+import ContributorNameLink from 'components/organisms/Contributor/ContributorNameLink';
 
-export default styled.h3`
-  margin: 0;
-  padding-bottom: 0;
-  font-size: ${props => props.theme.fontSizeDefault};
-  color: ${props => props.theme.activeColor};
+export default styled(UserName).attrs(() => ({
+  as: 'h3'
+}))`
+  margin-bottom: 10px;
 
-  display: table;
-  table-layout: fixed;
-  width: 100%;
-  white-space: nowrap;
-
-  ${Link} {
-    display: table-cell;
-    overflow: hidden;
-    font-weight: bold;
-    text-overflow: ellipsis;
-  }
-
-  @media (max-width: ${props => props.theme.tabletWidth}) {
-    font-size: 20px;
+  ${ContributorNameLink} {
+    display: block;
+    font-size: 16px;
+    white-space: normal;
   }
 `;
