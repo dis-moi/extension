d5636de90bb0b0bfc29c27b21cd398b021f00b8d
newick <erick@entre-quote.com>
2020-10-09 10:26:32 +0200

    fix(PrivacyMessageBox): removes useless <p>

--- src/components/molecules/SidebarBox/PrivacyMessageBox.tsx
+++ src/components/molecules/SidebarBox/PrivacyMessageBox.tsx
@@ -1,22 +1,20 @@
 import React from 'react';
-import { ExternalLink, List, Paragraph, Title2 } from 'components/atoms';
+import { ExternalLink, List, Title2 } from 'components/atoms';
 import SidebarBox from './SidebarBox';
 import { WEBSITE_DOMAIN } from 'app/lmem';
 
 export default () => (
   <SidebarBox>
     <Title2 as="h3">DisMoi respecte votre vie privée</Title2>
-    <Paragraph>
-      <List>
-        <li>
-          Nous ne collectons ni revendons{' '}
-          <strong>aucune donnée personnelle</strong>
-        </li>
-        <li>
-          Nous ne faisons <strong>aucun profilage</strong>
-        </li>
-      </List>
-    </Paragraph>
+    <List>
+      <li>
+        Nous ne collectons ni revendons{' '}
+        <strong>aucune donnée personnelle</strong>
+      </li>
+      <li>
+        Nous ne faisons <strong>aucun profilage</strong>
+      </li>
+    </List>
 
     <ExternalLink href={`https://${WEBSITE_DOMAIN}/vie-privee/`}>
       En savoir plus
