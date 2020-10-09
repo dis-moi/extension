eb937d28eccd198967f5de6f2c277125593dd4bf
newick <erick@entre-quote.com>
2020-10-09 12:44:01 +0200

    fix(tab-navigation): improves columns display

--- src/components/atoms/Layout/TwoColumns.ts
+++ src/components/atoms/Layout/TwoColumns.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const TwoColumns = styled.div`
   display: grid;
   grid-template-columns: minmax(0, 1fr) 310px;
-  grid-column-gap: ${props => props.theme.marginM};
+  grid-column-gap: 24px;
 
   @media (max-width: ${props => props.theme.desktopWidth}) {
     display: block;
