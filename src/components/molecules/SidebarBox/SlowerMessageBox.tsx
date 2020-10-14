cef268ea003ba23e3545883987101169562fc488
newick <erick@entre-quote.com>
2020-10-14 18:37:48 +0200

    fix(SlowerMessageBox): decreases title impact

--- src/components/molecules/SidebarBox/SlowerMessageBox.tsx
+++ src/components/molecules/SidebarBox/SlowerMessageBox.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import styled from 'styled-components';
 import { ExternalLink, Paragraph, Title2 } from 'components/atoms';
 import SidebarBox from './SidebarBox';
 import { WEBSITE_DOMAIN } from 'app/lmem';
 
+const Title = styled(Title2)`
+  font-weight: 700;
+  font-size: 18px;
+  color: ${props => props.theme.textColor};
+`;
+
 export default () => (
   <SidebarBox>
-    <Title2 as="h3">DisMoi ne ralentit pas votre navigateur</Title2>
+    <Title as="h3">DisMoi ne ralentit pas votre navigateur</Title>
     <Paragraph>
       Contrairement à d’autres extensions, DisMoi ne ralentit pas votre
       navigateur.
