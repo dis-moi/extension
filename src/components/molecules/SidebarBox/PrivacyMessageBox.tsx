157dbd77d15c1c14c5f5a2f5e6a7ce0b0e94a788
newick <erick@entre-quote.com>
2020-10-14 18:58:02 +0200

    fix(PrivacyMessageBox): decreases list impact

--- src/components/molecules/SidebarBox/PrivacyMessageBox.tsx
+++ src/components/molecules/SidebarBox/PrivacyMessageBox.tsx
@@ -1,20 +1,34 @@
 import React from 'react';
+import styled from 'styled-components';
 import { ExternalLink, List, Title2 } from 'components/atoms';
 import SidebarBox from './SidebarBox';
 import { WEBSITE_DOMAIN } from 'app/lmem';
 
+const Ul = styled(List)`
+  padding-left: 0;
+`;
+
+const ListItem = styled.li`
+  line-height: initial;
+  list-style-type: none;
+
+  & + & {
+    margin-top: 6px;
+  }
+`;
+
 export default () => (
   <SidebarBox>
     <Title2 as="h3">DisMoi respecte votre vie privée</Title2>
-    <List>
-      <li>
+    <Ul>
+      <ListItem>
         Nous ne collectons ni revendons{' '}
         <strong>aucune donnée personnelle</strong>
-      </li>
-      <li>
+      </ListItem>
+      <ListItem>
         Nous ne faisons <strong>aucun profilage</strong>
-      </li>
-    </List>
+      </ListItem>
+    </Ul>
 
     <ExternalLink href={`https://${WEBSITE_DOMAIN}/vie-privee/`}>
       En savoir plus
