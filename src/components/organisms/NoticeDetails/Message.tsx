f4b8d64de8c4ff5c0d3cbd72d260f2ef45afcbfc
newick <erick@entre-quote.com>
2020-11-26 16:55:30 +0100

    fix(DetailsContent): changes margins to improve readibility

--- src/components/organisms/NoticeDetails/Message.tsx
+++ src/components/organisms/NoticeDetails/Message.tsx
@@ -2,12 +2,12 @@ import React, { MouseEventHandler } from 'react';
 import styled from 'styled-components';
 
 const MessageBlock = styled.div`
-  max-height: 200px;
-  padding-top: 12px;
+  max-height: 216px;
+  margin-top: 10px;
   overflow-y: auto;
 
   p {
-    margin: 0;
+    margin: 0 16px 0 0;
     font-size: 15px;
     line-height: 1.4;
     color: ${props => props.theme.primaryColor};
