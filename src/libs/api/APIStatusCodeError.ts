6b1d35427ea61e145b70db4e2a9a9ef65675f104
lutangar <johan.dufour@gmail.com>
2021-01-13 14:07:58 +0100

    chore: Differentiate 4xx Http errors from an actual server error fix #360

--- src/libs/api/APIStatusCodeError.ts
+++ src/libs/api/APIStatusCodeError.ts
@@ -3,7 +3,7 @@ export class APIStatusCodeError extends Error {
   public readonly statusCode: number;
 
   constructor(response: Response) {
-    super('Bad response from server');
+    super(response.statusText || 'Bad response from server');
     this.name = 'APIStatusCodeError';
     this.response = response;
     this.statusCode = response.status;
