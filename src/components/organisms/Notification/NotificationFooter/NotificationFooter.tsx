f2e3ebbabe4d092ef162d02ccc84dea1c6ab2928
Lacoste <gregoire@level-one.fr>
2021-03-01 14:48:43 +0100

    feat(i18n): setup i18next extension

--- src/components/organisms/Notification/NotificationFooter/NotificationFooter.tsx
+++ src/components/organisms/Notification/NotificationFooter/NotificationFooter.tsx
@@ -5,21 +5,25 @@ import {
   FooterRoute,
   footerRoutes
 } from '../../../../app/content/footerRoutes';
+import { useTranslation } from 'react-i18next';
 
 interface NotificationFooterProps {
   children?: FooterRoute[];
 }
 
-const NotificationFooter = ({ children }: NotificationFooterProps) => (
-  <FooterContainer>
-    {children &&
-      children.map(route => (
-        <NavLink key={route.location as string} to={route.location}>
-          {route.element}
-        </NavLink>
-      ))}
-  </FooterContainer>
-);
+const NotificationFooter = ({ children }: NotificationFooterProps) => {
+  const { t } = useTranslation();
+  return (
+    <FooterContainer>
+      {children &&
+        children.map(route => (
+          <NavLink key={route.location as string} to={route.location}>
+            {t(route.element)}
+          </NavLink>
+        ))}
+    </FooterContainer>
+  );
+};
 
 NotificationFooter.defaultProps = {
   children: footerRoutes
