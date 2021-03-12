4b28e3f1bca56f3fea141f393ff7f7c2f441d6fc
Lacoste <gregoire@level-one.fr>
2021-03-12 14:45:17 +0100

    feat(i18n): translate NotificationFooter

--- src/components/organisms/Notification/NotificationFooter/NotificationFooter.tsx
+++ src/components/organisms/Notification/NotificationFooter/NotificationFooter.tsx
@@ -1,21 +1,24 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import FooterContainer from './FooterContainer';
 import NavLink from './NavLink';
 import {
   FooterRoute,
   footerRoutes
 } from '../../../../app/content/footerRoutes';
+
 interface NotificationFooterProps {
   children?: FooterRoute[];
 }
 
 const NotificationFooter = ({ children }: NotificationFooterProps) => {
+  const { t } = useTranslation();
   return (
     <FooterContainer>
       {children &&
         children.map(route => (
           <NavLink key={route.location as string} to={route.location}>
-            {route.element}
+            {t(route.i18nKey)}
           </NavLink>
         ))}
     </FooterContainer>
