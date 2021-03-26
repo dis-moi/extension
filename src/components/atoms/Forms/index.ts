4571203c5f5c4be1a2ba2acc83859b534e922f0b
newick <erick@entre-quote.com>
2021-03-26 13:52:47 +0100

    feat(Contribute): adds labels atom

--- src/components/atoms/Forms/index.ts
+++ src/components/atoms/Forms/index.ts
@@ -3,3 +3,4 @@ export { default as Input } from './Input/Input';
 export { default as Error } from './Error/Error';
 export { default as Textarea } from './Textarea/Textarea';
 export { default as Select } from './Select/Select';
+export { default as Label } from './Label/Label';
