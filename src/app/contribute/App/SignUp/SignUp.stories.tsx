ddfa2f3570461962c5908bf61367325eff9b76fc
newick <erick@entre-quote.com>
2021-03-26 15:40:18 +0100

    feat(Contribute): adds SignUp content

---
+++ src/app/contribute/App/SignUp/SignUp.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import SignUp from './SignUp';
+
+export default { title: 'Contribute/SignUp' };
+
+export const Normal = () => <SignUp />;
+export const Error = () => <SignUp />;
+export const Valid = () => <SignUp />;
+export const SentEmailMessage = () => <SignUp />;
