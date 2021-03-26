9e5fe1c58700703410e657d767ef0218232cd933
newick <erick@entre-quote.com>
2021-03-26 14:41:06 +0100

    feat(Contribute): fixes label atom name in Storybook

--- src/components/atoms/Forms/Label/Label.stories.tsx
+++ src/components/atoms/Forms/Label/Label.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Label from './Label';
 
-export default { title: 'atoms/Forms/Label' };
+export default { title: 'Components/atoms/Forms/Label' };
 
 export const normal = () => <Label>A nice label</Label>;
