085cef6c74c71766358cff3aaa30b7d3c6e00537
Gregoire <gregoire@level-one.fr>
2021-04-01 16:16:31 +0200

    feat(NoticesList): add shadow for more than 2 contributions

--- src/components/atoms/AddNoticeContainer.ts
+++ src/components/atoms/AddNoticeContainer.ts
@@ -1,11 +1,15 @@
 import styled from 'styled-components';
 import CenterContainer from './CenterContainer';
 
-export default styled(CenterContainer)`
+interface AddNoticeContainerProps {
+  shadow?: boolean;
+}
+
+export default styled(CenterContainer)<AddNoticeContainerProps>`
   width: 100%;
   position: absolute;
   // bottom: 60px; // In case of ServiceMessageLine
-  bottom: 16px;
-  box-shadow: 0 -4px 4px rgba(0, 0, 0, 0.4);
-  padding-top: 18px;
+  bottom: 14px;
+  box-shadow: ${props => (props.shadow ? '0 -2px 2px rgba(0, 0, 0, 0.4)' : '')};
+  padding-top: 14px;
 `;
