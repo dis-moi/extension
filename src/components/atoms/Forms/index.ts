81289e9d4f9b0248d644ff7977bcfa5b9359cb86
newick <erick@entre-quote.com>
2021-04-01 14:29:32 +0200

    feat(Contribute): adds CreateContributor content and adjusts other screens

--- src/components/atoms/Forms/index.ts
+++ src/components/atoms/Forms/index.ts
@@ -4,3 +4,5 @@ export { default as Error } from './Error/Error';
 export { default as Textarea } from './Textarea/Textarea';
 export { default as Select } from './Select/Select';
 export { default as Label } from './Label/Label';
+export { default as FormGroup } from './FormGroup';
+export { default as CheckboxLine } from './CheckboxLine';
