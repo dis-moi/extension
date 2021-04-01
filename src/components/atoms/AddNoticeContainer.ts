aacc6aa3dac737aa79f1272f488d782462ca64f0
newick <erick@entre-quote.com>
2021-04-01 12:14:16 +0200

    fix(NoticesList): adds some box-shadow

--- src/components/atoms/AddNoticeContainer.ts
+++ src/components/atoms/AddNoticeContainer.ts
@@ -6,4 +6,6 @@ export default styled(CenterContainer)`
   position: absolute;
   // bottom: 60px; // In case of ServiceMessageLine
   bottom: 16px;
+  box-shadow: 0 -4px 4px rgba(0, 0, 0, 0.4);
+  padding-top: 18px;
 `;
