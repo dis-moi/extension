a30ce340d7e59fa9efac096f37938033676f42b7
newick <erick@entre-quote.com>
2021-04-01 17:33:06 +0200

    feat(Contribute): adds ResetPassword content and adjusts other screens

---
+++ src/app/contribute/App/ForgotPassword/SentLink.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Paragraph, Title } from 'components/atoms';
+import { Wrapper } from '../components';
+
+const SentLink = () => {
+  return (
+    <Wrapper>
+      <Title>Password reset</Title>
+
+      <Paragraph>Please check your email and follow the steps.</Paragraph>
+    </Wrapper>
+  );
+};
+
+export default SentLink;
