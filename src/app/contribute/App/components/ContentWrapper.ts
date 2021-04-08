eb153464ee922e42ebb33d24629dadf13f36394f
newick <erick@entre-quote.com>
2021-04-08 18:38:41 +0200

    feat(Contribute): adds some styles

---
+++ src/app/contribute/App/components/ContentWrapper.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const ContentWrapper = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  padding: 36px 58px;
+  text-align: center;
+`;
+
+export default ContentWrapper;
