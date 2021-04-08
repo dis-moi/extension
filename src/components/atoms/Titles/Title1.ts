7f042ce57bd006d0852e25af8720295675523ca0
newick <erick@entre-quote.com>
2021-04-08 16:44:01 +0200

    chore(StorybookContent): adds default titles

---
+++ src/components/atoms/Titles/Title1.ts
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+const Title1 = styled.h1``;
+
+export default Title1;
--- src/components/atoms/Titles/index.ts
+++ src/components/atoms/Titles/index.ts
@@ -1,1 +1,4 @@
+export { default as Title1 } from './Title1';
 export { default as Title2 } from './Title2';
+export { default as Title3 } from './Title3';
+export { default as Title4 } from './Title4';
