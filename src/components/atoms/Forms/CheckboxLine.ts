11b19a93cb7f9c9ab0b141ef18fd66d12bb4ad38
newick <erick@entre-quote.com>
2021-04-15 11:57:16 +0200

    feat(Contribute): styles elements

--- src/components/atoms/Forms/CheckboxLine.ts
+++ src/components/atoms/Forms/CheckboxLine.ts
@@ -1,8 +1,16 @@
 import styled from 'styled-components';
+import { Label } from './index';
 
 const CheckboxLine = styled.div`
+  display: flex;
+  align-items: center;
+
   &:not(:last-of-type) {
     margin-bottom: 8px;
+  }
+
+  ${Label} {
+    margin-left: 8px;
   }
 `;
 
