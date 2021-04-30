e0b75b1c5a44b299bc61d6783f3dd5fef87ae196
Gregoire <gregoire@level-one.fr>
2021-04-30 18:46:16 +0200

    chore(storybook): support mdx and auto documentation

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const rules = require('../webpack/config.rules');
 const basePlugins = require('../webpack/config.plugins.base');
+const createCompiler = require('@storybook/addon-docs/mdx-compiler-plugin');
 
 const env = { build: 'dev' };
 const argv = {
@@ -20,11 +21,39 @@ module.exports = ({ config }) => {
         name: '[path][name].[ext]',
         context: 'src/assets'
       }
+    },
+    {
+      // 2a. Load `.stories.mdx` / `.story.mdx` files as CSF and generate
+      //     the docs page from the markdown
+      test: /\.(stories|story)\.mdx$/,
+      use: [
+        {
+          loader: 'babel-loader',
+          // may or may not need this line depending on your app's setup
+          options: {
+            plugins: ['@babel/plugin-transform-react-jsx']
+          }
+        },
+        {
+          loader: '@mdx-js/loader',
+          options: {
+            compilers: [createCompiler({})]
+          }
+        }
+      ]
+    },
+    {
+      // 2b. Run `source-loader` on story files to show their source code
+      //     automatically in `DocsPage` or the `Source` doc block.
+
+      test: /\.(stories|story)\.[tj]sx?$/,
+      loader: require.resolve('@storybook/source-loader'),
+      exclude: [/node_modules/],
+      enforce: 'pre'
     }
   ];
   config.resolve.modules.push(path.resolve(__dirname, '..', 'src'));
   config.resolve.alias.test = path.resolve(__dirname, '..', 'test');
-  //migrate v5->6: Zero config typescript https://github.com/storybookjs/storybook/blob/next/MIGRATION.md#zero-config-typescript
   const basePluginsArray = basePlugins(env, 'development');
   config.plugins.push(...basePluginsArray);
   config.stats = require('../webpack/config.stats');
