2abcd3549461d68b713bb6afbe772e559503a686
Gregoire <gregoire@level-one.fr>
2021-05-14 07:23:42 +0200

    fix(onBoardingRestart): memoize onboarding

--- src/app/utils/isRedirectedFromExtensionInstall.ts
+++
@@ -1,6 +0,0 @@
-const REDIRECTED_PATH = 'pk_campaign=installed';
-
-const isRedirectedFromExtensionInstall = window.location.search.includes(
-  REDIRECTED_PATH
-);
-export default isRedirectedFromExtensionInstall;
