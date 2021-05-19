43eb261f4c8731a660247aabaa85507e6455a723
Gregoire <gregoire@level-one.fr>
2021-05-19 09:41:51 +0200

    test(i18n): add test translation file

---
+++ src/i18n/i18n.spec.ts
@@ -0,0 +1,23 @@
+import { expect } from 'chai';
+import { options } from './index';
+import i18n from './';
+
+describe('i18n translation', () => {
+  beforeEach(() => {
+    i18n.init(options).then(() => {});
+  });
+  const testCreationDate = new Date(2021, 4 /* may */, 10);
+  it('common.or return or', () => {
+    expect(i18n.t('common.or')).to.equal('or');
+  });
+  it('return format short version en', () => {
+    expect(i18n.t('date.short', { date: testCreationDate })).to.equal(
+      '05/10/2021'
+    );
+  });
+  it('return format short version en from profiles NS', () => {
+    expect(
+      i18n.t('profiles:notice.since', { date: testCreationDate })
+    ).to.equal('Since 05/10/2021');
+  });
+});
