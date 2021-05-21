90701b1e757105c8d42b5a0f20322fc0de0ba60d
newick <erick@entre-quote.com>
2021-05-21 15:41:30 +0200

    feat(Contribute): adds more style to dashboard

---
+++ src/components/atoms/icons/Message.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Follower = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 35.833 35.833">
+    <circle
+      cx="1.493"
+      cy="1.493"
+      r="1.493"
+      transform="translate(16.57 16.57)"
+    />
+    <circle
+      cx="1.493"
+      cy="1.493"
+      r="1.493"
+      transform="translate(9.321 16.57)"
+    />
+    <circle
+      cx="1.493"
+      cy="1.493"
+      r="1.493"
+      transform="translate(23.819 16.57)"
+    />
+    <path d="M17.917 0A17.919 17.919 0 002.376 26.836L.068 34a1.4 1.4 0 001.761 1.761L9 33.458A17.917 17.917 0 1017.917 0zm0 33.034a15.073 15.073 0 01-8-2.29 1.4 1.4 0 00-1.173-.144l-5.175 1.665 1.666-5.175a1.4 1.4 0 00-.146-1.171 15.118 15.118 0 1112.827 7.115z" />
+  </svg>
+);
+
+export default Follower;
