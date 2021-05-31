74f7e22fbbdf2005e2326e3bb797cb1b0284a1c1
lutangar <johan.dufour@gmail.com>
2021-05-31 11:57:13 +0200

    chore(i18n): Use isMatch to validate date format and don't silence the format error.

--- src/i18n/format.ts
+++ src/i18n/format.ts
@@ -1,19 +1,24 @@
-import { format as dateFormat, parseISO, isDate } from 'date-fns';
+import { format as dateFormat, parseISO, isDate, isMatch } from 'date-fns';
 import { getLocale as getDateLocale, DEFAULT_FORMAT } from './date';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export default function(value: any, format?: string, lng?: string) {
   const isoDate = typeof value === 'string' ? parseISO(value) : value;
   if (isDate(isoDate)) {
-    try {
-      return dateFormat(isoDate as Date, format || DEFAULT_FORMAT, {
-        locale: getDateLocale(lng)
-      });
-    } catch (e) {
-      return dateFormat(isoDate as Date, DEFAULT_FORMAT, {
-        locale: getDateLocale(lng)
-      });
+    const formatValid = format && isMatch(isoDate, format);
+    if (!formatValid) {
+      console.error(`Format ${format} is invalid, see date-fns docs for a list of valid formats:
+https://date-fns.org/v2.22.1/docs/format
+`);
     }
+
+    return dateFormat(
+      isoDate as Date,
+      formatValid ? (format as string) : DEFAULT_FORMAT,
+      {
+        locale: getDateLocale(lng)
+      }
+    );
   }
 
   return value;
