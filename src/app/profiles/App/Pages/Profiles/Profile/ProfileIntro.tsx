922ebe83d480fe59524d5e85aa0aca9dd409ab1a
newick <erick@entre-quote.com>
2021-06-03 14:23:11 +0200

    fix(ProfileIntro): simplifies code

--- src/app/profiles/App/Pages/Profiles/Profile/ProfileIntro.tsx
+++ src/app/profiles/App/Pages/Profiles/Profile/ProfileIntro.tsx
@@ -125,7 +125,7 @@ export const ProfileIntro = ({
           loading={loading}
           avatarSize="large"
           usernameAs={usernameAs}
-        ></ContributorLarge>
+        />
       </ProfileIntroContent>
     </>
   );
