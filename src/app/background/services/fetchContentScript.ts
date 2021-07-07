d7c3aedb079c5d91746e8082c45d85a4b461500f
Jalil Arfaoui <jalil@arfaoui.net>
2021-07-07 19:42:19 +0200

    fix: remove unused file

--- src/app/background/services/fetchContentScript.ts
+++
@@ -1,6 +0,0 @@
-const getText = (response: Response): Promise<string> => response.text();
-
-export const fetchContentScript = (input: string): Promise<string> =>
-  fetch(`.${input}`).then(getText);
-
-export default fetchContentScript;
