55c426e373e32ce4317ed3a49e40c35e31e70383
newick <erick@entre-quote.com>
2021-09-27 14:37:19 +0200

    fix(profilesList): changes list item width

--- src/components/atoms/Contributor/ContributorInfos.ts
+++ src/components/atoms/Contributor/ContributorInfos.ts
@@ -1,5 +1,4 @@
 import styled from 'styled-components';
-import UserName from '../UserName/UserName';
 
 export default styled.div`
   flex-grow: 1;
@@ -7,8 +6,4 @@ export default styled.div`
   flex-direction: column;
   justify-content: center;
   margin-left: 15px;
-
-  ${UserName} {
-    margin-bottom: 10px;
-  }
 `;
--- src/components/organisms/Contributor/ContributorsList.ts
+++ src/components/organisms/Contributor/ContributorsList.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 const ContributorsList = styled.section`
   display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  grid-template-columns: repeat(auto-fill, minmax(450px, 1fr));
   grid-gap: 24px;
 
   @media (max-width: ${props => props.theme.desktopWidth}) {
