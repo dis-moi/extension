a415ac776a93a16e144f902264985486f0d169e8
Antoine Wolff <antoine.wolff@arza-studio.com>
2021-12-15 15:20:34 +0100

    Change s éclairer for s’éclairer

--- src/app/website/gatsby-config.js
+++ src/app/website/gatsby-config.js
@@ -11,7 +11,7 @@ module.exports = {
   siteMetadata: {
     title: process.env.FACET_NAME || 'DisMoi',
     description: `Notre projet est de **réinstaurer la liberté d’information partout sur le web**, afin de le rendre plus sûr, plus transparent et plus favorable aux alternatives. DisMoi est un réseau en surcouche du web qui permet aux internautes de **se conseiller et de
-s'éclairer directement sur les pages du web qu’ils visitent**.`,
+s’éclairer directement sur les pages du web qu’ils visitent**.`,
     author: `Dismoi Team`,
     siteUrl: process.env.WEBSITE_URL || 'https://'
   },
