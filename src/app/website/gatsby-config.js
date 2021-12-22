f23fa2c986dd5bb56c58282f384186d759209df8
Jalil Arfaoui <jalil@arfaoui.net>
2021-12-22 01:38:32 +0100

    fix(website): add gatsby-plugin-styled-components to fix missing style

--- src/app/website/gatsby-config.js
+++ src/app/website/gatsby-config.js
@@ -16,6 +16,7 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-styled-components',
     `gatsby-plugin-image`,
     // {
     //   resolve: 'gatsby-plugin-copy-files-enhanced',
