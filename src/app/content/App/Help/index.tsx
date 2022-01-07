198435987d3a0d7da9d7783f40cd0eb72a226b68
Gregoire <gregoire.lacoste@gmail.com>
2022-01-07 10:24:44 +0100

    feat: do note display faq on lmel  #1209

--- src/app/content/App/Help/index.tsx
+++ src/app/content/App/Help/index.tsx
@@ -11,33 +11,44 @@ export const Help = () => {
   const { t } = useTranslation();
   return (
     <Container>
-      <ContentTitle>{t('faq.title')}</ContentTitle>
-      <List>
-        <li>
-          <ExternalLink href={`https://${WEBSITE_DOMAIN}${t('path.appear')}/`}>
-            {t('faq.notices.where_appear')}
-          </ExternalLink>
-        </li>
-        <li>
-          <ExternalLink
-            href={`https://${WEBSITE_DOMAIN}${t(
-              'path.problem_post_appearing'
-            )}/`}
-          >
-            {t('faq.notices.dont_appear')}
-          </ExternalLink>
-        </li>
-        <li>
-          <ExternalLink href={`https://${WEBSITE_DOMAIN}${t('path.privacy')}/`}>
-            {t('faq.privacy.used_data')}
-          </ExternalLink>
-        </li>
-        <li>
-          <ExternalLink href={`https://${WEBSITE_DOMAIN}${t('path.help')}/`}>
-            {t('faq.other_questions.title')}
-          </ExternalLink>
-        </li>
-      </List>
+      {process.env.FACET !== 'lmel' && (
+        <>
+          {' '}
+          <ContentTitle>{t('faq.title')}</ContentTitle>
+          <List>
+            <li>
+              <ExternalLink
+                href={`https://${WEBSITE_DOMAIN}${t('path.appear')}/`}
+              >
+                {t('faq.notices.where_appear')}
+              </ExternalLink>
+            </li>
+            <li>
+              <ExternalLink
+                href={`https://${WEBSITE_DOMAIN}${t(
+                  'path.problem_post_appearing'
+                )}/`}
+              >
+                {t('faq.notices.dont_appear')}
+              </ExternalLink>
+            </li>
+            <li>
+              <ExternalLink
+                href={`https://${WEBSITE_DOMAIN}${t('path.privacy')}/`}
+              >
+                {t('faq.privacy.used_data')}
+              </ExternalLink>
+            </li>
+            <li>
+              <ExternalLink
+                href={`https://${WEBSITE_DOMAIN}${t('path.help')}/`}
+              >
+                {t('faq.other_questions.title')}
+              </ExternalLink>
+            </li>
+          </List>
+        </>
+      )}
 
       <ContentTitle>{t('view.help.contact_support')}</ContentTitle>
       <Content>
