d9d6e26a6a9950f81b5e6f8adfe62ed30c38acc4
Jalil Arfaoui <jalil@arfaoui.net>
2022-01-13 13:56:54 +0100

    chore: include yarn install in ci/createVersion.sh

--- ci/createVersion.sh
+++ ci/createVersion.sh
@@ -1,5 +1,6 @@
 if [[ $(git log --format=%B -n 1) == "chore: release"* ]]; then
   echo "version commit, all clear"
 else
+  yarn install --frozen-lockfile --production=false
   yarn semantic-release
 fi
